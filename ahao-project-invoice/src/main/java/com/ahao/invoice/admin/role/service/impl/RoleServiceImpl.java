c7c29bc74dd63d9c05e279808c37d9afcbc1c6c0
Ahaochan <844394093@qq.com>
2017-07-23 20:02:00 +0800

    完成权限验证, 添加增删功能

--- ahao-project-invoice/src/main/java/com/ahao/invoice/admin/role/service/impl/RoleServiceImpl.java
+++ ahao-project-invoice/src/main/java/com/ahao/invoice/admin/role/service/impl/RoleServiceImpl.java
@@ -1,11 +1,10 @@
 package com.ahao.invoice.admin.role.service.impl;
 
-import com.ahao.context.PageContext;
 import com.ahao.invoice.admin.role.dao.RoleDAO;
 import com.ahao.invoice.admin.role.entity.RoleDO;
 import com.ahao.invoice.admin.role.service.RoleService;
-import com.ahao.service.impl.DataServiceImpl;
 import com.ahao.service.impl.PageServiceImpl;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Avalon on 2017/6/3.
@@ -41,6 +41,15 @@ public class RoleServiceImpl extends PageServiceImpl<RoleDO> implements RoleServ
     }
 
     @Override
+    public Map<RoleDO, Boolean> getSelectedRole(String... roleIds) {
+        Long[] ids = Stream.of(roleIds).map(Long::parseLong).toArray(Long[]::new);
+        Set<RoleDO> allRole = roleDAO.selectAllNameAndEnabled();
+        Map<RoleDO, Boolean> roles = allRole.stream()
+                .collect(Collectors.toMap(r -> r, r -> ArrayUtils.contains(ids, r.getId())));
+        return roles;
+    }
+
+    @Override
     public Map<RoleDO, Boolean> getSelectedRole(Long userId) {
         if(userId==null) {
             logger.warn("用户id不能为空");
@@ -54,11 +63,17 @@ public class RoleServiceImpl extends PageServiceImpl<RoleDO> implements RoleServ
     }
 
     @Override
-    public void addRelate(Long userId, Long[] roleIds) {
+    public void addRelate(Long userId, String[] roleIds) {
         if(userId == null || roleIds==null || roleIds.length<=0){
             logger.warn("用户角色表添加失败, 用户id或角色id为空");
             return;
         }
-        roleDAO.addRelate(userId, roleIds);
+        Long[] ids = Stream.of(roleIds).map(Long::parseLong).toArray(Long[]::new);
+        roleDAO.addRelate(userId, ids);
+    }
+
+    @Override
+    public int deleteByKey(Object roleId) {
+        return roleDAO.deleteByKey((Long) roleId);
     }
 }
