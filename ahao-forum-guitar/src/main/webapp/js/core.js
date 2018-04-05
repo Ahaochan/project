0f06a7b42894b8697bf6049cccf6741077888162
Ahaochan <844394093@qq.com>
2018-04-05 14:34:53 +0800

    添加contains方法

--- ahao-forum-guitar/src/main/webapp/js/core.js
+++ ahao-forum-guitar/src/main/webapp/js/core.js
@@ -9,3 +9,13 @@ $(function () {
         });
     })(jQuery);
 });
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] == obj) {
+            return true;
+        }
+    }
+    return false;
+};
