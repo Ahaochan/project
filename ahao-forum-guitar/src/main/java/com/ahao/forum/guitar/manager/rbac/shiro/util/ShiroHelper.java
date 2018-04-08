35f0b82504d18683573aaf99828e38bbdd53f452
Ahaochan <844394093@qq.com>
2018-04-08 20:35:22 +0800

    ShiroHelper添加判断角色的方法

--- ahao-forum-guitar/src/main/java/com/ahao/forum/guitar/manager/rbac/shiro/util/ShiroHelper.java
+++ ahao-forum-guitar/src/main/java/com/ahao/forum/guitar/manager/rbac/shiro/util/ShiroHelper.java
@@ -3,6 +3,7 @@ package com.ahao.forum.guitar.manager.rbac.shiro.util;
 import com.ahao.core.config.SpringConfig;
 import com.ahao.core.entity.DataSet;
 import com.ahao.core.entity.IDataSet;
+import com.ahao.core.util.lang.CollectionHelper;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,24 +27,29 @@ public class ShiroHelper {
         return getCurrentUser().getInt("weight");
     }
 
+    public static int getMaxWeight(){
+        String sql = "select max(IFNULL(r.weight, 0)) weight from admin_role r where r.enabled = 1";
+        return new DataSet(dao().queryForMap(sql)).getInt("weight");
+    }
+
     public static boolean isRoot(){
         int maxWeight = getMaxWeight();
         int weight = ShiroHelper.getMyUserWeight();
         return weight >= maxWeight;
     }
 
-    public static int getMaxWeight(){
-        String sql = "select max(IFNULL(r.weight, 0)) weight from admin_role r where r.enabled = 1";
-        return new DataSet(dao().queryForMap(sql)).getInt("weight");
-    }
-
     public static boolean hasAllAuths(String... authNames){
         return getSubject().isPermittedAll(authNames);
+    }
+
+    public static boolean hasAllRoles(String... roleNames){
+        return getSubject().hasAllRoles(CollectionHelper.toList(roleNames));
     }
 
     public static IDataSet getCurrentUser(){
         return (IDataSet) getSubject().getPrincipal();
     }
+
 
     private static Subject getSubject(){
         return SecurityUtils.getSubject();
