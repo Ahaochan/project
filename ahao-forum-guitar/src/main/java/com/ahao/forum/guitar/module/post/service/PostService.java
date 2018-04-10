a635cad396505ab8cf5b1262c7067168c235ced9
Ahaochan <844394093@qq.com>
2018-04-10 22:25:03 +0800

    删除无用代码

--- ahao-forum-guitar/src/main/java/com/ahao/forum/guitar/module/post/service/PostService.java
+++
@@ -1,9 +0,0 @@
-package com.ahao.forum.guitar.module.post.service;
-
-
-import com.alibaba.fastjson.JSONObject;
-
-public interface PostService {
-
-    JSONObject getPosts(Long categoryId, String search, Integer page);
-}
