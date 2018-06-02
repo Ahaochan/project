c2e24a5b03f88aa27d1484ee0ff3be73d8345740
Ahaochan <844394093@qq.com>
2018-06-02 15:47:21 +0800

    分页参数转为从session中获取

--- ahao-core/src/main/java/com/ahao/core/context/PageContext.java
+++ ahao-core/src/main/java/com/ahao/core/context/PageContext.java
@@ -1,66 +1,51 @@
 package com.ahao.core.context;
 
+import com.ahao.core.util.web.SessionHelper;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * 分页参数
- * 
+ *
  * @author Ahaochan
  */
 public class PageContext {
-	public static final String PAGE = "page";
-	public static final String PAGE_SIZE = "pageSize";
-	public static final String ORDER = "order";
-	public static final String SORT = "sort";
+    public static final String PAGE = "page";
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String ORDER = "order";
+    public static final String SORT = "sort";
 
-	public static final int DEFAULT_PAGE_SIZE = 25;
-	public static final String DEFAULT_ORDER = "asc";
-	public static final String DEFAULT_SORT = "id";
+    public static final int DEFAULT_PAGE_SIZE = 25;
+    public static final String DEFAULT_ORDER = "asc";
+    public static final String DEFAULT_SORT = "id";
 
-	/**
-	 * 分页大小
-	 */
-	private static ThreadLocal<Integer> pageSize = new ThreadLocal<Integer>();
-	/**
-	 * 升序还是降序
-	 */
-	private static ThreadLocal<String> order = new ThreadLocal<String>();
-	/**
-	 * 根据那个字段排序
-	 */
-	private static ThreadLocal<String> sort = new ThreadLocal<String>();
+    public static String getOrder() {
+        HttpSession session = SessionHelper.getSession();
+        return SessionHelper.get(ORDER, DEFAULT_ORDER, session);
+    }
 
-	public static String getOrder() {
-		return order.get();
-	}
+    public static void setOrder(String order) {
+        HttpSession session = SessionHelper.getSession();
+        SessionHelper.set(ORDER, order, session);
+    }
 
-	public static void setOrder(String _order) {
-		order.set(_order);
-	}
+    public static String getSort() {
+        HttpSession session = SessionHelper.getSession();
+        return SessionHelper.get(SORT, DEFAULT_SORT, session);
+    }
 
-	public static void removeOrder() {
-		order.remove();
-	}
+    public static void setSort(String sort) {
+        HttpSession session = SessionHelper.getSession();
+        SessionHelper.set(SORT, sort, session);
+    }
 
-	public static String getSort() {
-		return sort.get();
-	}
+    public static void setPageSize(int pageSize) {
+        HttpSession session = SessionHelper.getSession();
+        SessionHelper.set(PAGE_SIZE, pageSize, session);
+    }
 
-	public static void setSort(String _sort) {
-		sort.set(_sort);
-	}
-
-	public static void removeSort() {
-		sort.remove();
-	}
-
-	public static void setPageSize(int _pageSize) {
-		pageSize.set(_pageSize);
-	}
-
-	public static int getPageSize() {
-		return pageSize.get();
-	}
-
-	public static void removePageSize() {
-		pageSize.remove();
-	}
+    public static int getPageSize() {
+        HttpSession session = SessionHelper.getSession();
+        return SessionHelper.get(PAGE_SIZE, DEFAULT_PAGE_SIZE, session);
+    }
 }
