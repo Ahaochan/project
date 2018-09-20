02b69f001d2bb73ad78fbf6bba8dcb70438c24f1
Ahaochan <844394093@qq.com>
2018-09-20 20:56:49 +0800

    重构 AjaxDTO , 使用链式构造

--- ahao-core-commons/src/main/java/com/ahao/commons/entity/AjaxDTO.java
+++ ahao-core-commons/src/main/java/com/ahao/commons/entity/AjaxDTO.java
@@ -2,89 +2,67 @@ package com.ahao.commons.entity;
 
 /**
  * Created by Ahaochan on 2017/7/18.
- *
  * 传递Ajax的Data Transfer Object
  */
 public class AjaxDTO {
-    public static final int FAILURE = 0;
-    public static final int SUCCESS = 1;
+    private static final int FAILURE = 0;
+    private static final int SUCCESS = 1;
 
     public static AjaxDTO failure(String msg) {
         return new AjaxDTO(FAILURE, msg, null);
     }
 
-    public static AjaxDTO success(Object obj) {
-        return success(null, obj);
+    public static AjaxDTO failure(Object obj) {
+        return new AjaxDTO(FAILURE, "", obj);
     }
 
-    public static AjaxDTO success(String msg, Object obj) {
-        return new AjaxDTO(SUCCESS, msg, obj);
+    public static AjaxDTO success(String msg) {
+        return new AjaxDTO(SUCCESS, msg, null);
+    }
+
+    public static AjaxDTO success(Object obj) {
+        return new AjaxDTO(SUCCESS, "", obj);
     }
 
     public static AjaxDTO get(boolean success) {
         return new AjaxDTO(success ? SUCCESS : FAILURE, "", null);
     }
 
-    public static AjaxDTO get(boolean success, String msg) {
-        return new AjaxDTO(success ? SUCCESS : FAILURE, msg, null);
-    }
 
-    public static AjaxDTO get(boolean success, String msg, Object obj) {
-        return new AjaxDTO(success ? SUCCESS : FAILURE, msg, obj);
-    }
-
-    public static AjaxDTO get(int result) {
-        return new AjaxDTO(result, "", null);
-    }
-
-    public static AjaxDTO get(int result, String msg) {
-        return new AjaxDTO(result, msg, null);
-    }
-
-    public static AjaxDTO get(int result, String msg, Object obj) {
-        return new AjaxDTO(result, msg, obj);
-    }
-
-    /**
-     * 0表示失败 1表示成功
-     */
     private int result;
-    /**
-     * 提示信息
-     */
     private String msg;
-    /**
-     * 附加对象，用来存储一些特定的返回信息
-     */
     private Object obj;
 
-    public AjaxDTO(int result, String msg, Object obj) {
+    private AjaxDTO(int result, String msg, Object obj) {
         this.result = result;
         this.msg = msg;
         this.obj = obj;
+    }
+
+    public AjaxDTO obj(Object obj) {
+        this.obj = obj;
+        return this;
+    }
+
+    public AjaxDTO result(int result) {
+        this.result = result;
+        return this;
+    }
+
+    public AjaxDTO msg(String msg) {
+        this.msg = msg;
+        return this;
     }
 
     public int getResult() {
         return result;
     }
 
-    public void setResult(int result) {
-        this.result = result;
-    }
-
     public String getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
     public Object getObj() {
         return obj;
-    }
-
-    public void setObj(Object obj) {
-        this.obj = obj;
     }
 }
