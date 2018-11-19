966674d4e46bc0fce28a1baba8b6093150831e82
Ahaochan <844394093@qq.com>
2018-11-19 12:32:53 +0800

    简单的spring boot 和 quartz 整合

---
+++ ahao-spring-quartz/src/main/java/com/ahao/spring/beans/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,28 @@
+package com.ahao.spring.beans;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+/**
+ * 将 Spring Bean 注入(Autowired) Quartz Job 中
+ * https://stackoverflow.com/a/15211030/6335926
+ */
+public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private transient AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+}
---
+++ ahao-spring-quartz/src/main/java/com/ahao/spring/quartz/config/QuartzConfig.java
@@ -0,0 +1,93 @@
+package com.ahao.spring.quartz.config;
+
+import com.ahao.core.spring.util.SpringContextHolder;
+import com.ahao.spring.beans.AutowiringSpringBeanJobFactory;
+import com.ahao.spring.quartz.config.properties.QuartzProperties;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Properties;
+
+@Configuration
+@DependsOn("com.ahao.core.spring.util.SpringContextHolder")
+@ConditionalOnProperty(name = "spring.quartz.enabled", havingValue = "true")
+public class QuartzConfig {
+    private static final Logger logger = LoggerFactory.getLogger(QuartzConfig.class);
+
+    @Autowired
+    private QuartzProperties quartzProperties;
+
+    @Bean
+    @Lazy
+    public SchedulerFactoryBean quartzScheduler(ApplicationContext applicationContext) throws Exception {
+        SchedulerFactoryBean factory = new SchedulerFactoryBean();
+        factory.setSchedulerName("solr-quartz-scheduler");
+
+        // 1. 初始化 quartz.properties 配置
+        factory.setQuartzProperties(quartzProperties());
+        factory.afterPropertiesSet();
+
+        // 2. 自定义 Job Factroy, 允许使用 @Autowired 进行注入
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        factory.setJobFactory(jobFactory);
+
+        // 3. 配置 触发器
+        Trigger[] triggers = quartzProperties.getCronTask().entrySet().stream()
+                .peek(e -> logger.debug("创建定时任务{}, cron:{}", e.getKey(), e.getValue()))
+                .map(e -> createTrigger(e.getKey(), e.getValue()))
+                .toArray(Trigger[]::new);
+        factory.setTriggers(triggers);
+
+        return factory;
+    }
+
+    @Bean
+    public Properties quartzProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+        propertiesFactoryBean.afterPropertiesSet();
+        Properties properties = propertiesFactoryBean.getObject();
+        return properties;
+    }
+
+    private Trigger createTrigger(String beanName, String cronExpression) {
+        try {
+            // 1. 创建 jobDetail
+            MethodInvokingJobDetailFactoryBean jobDetail = new MethodInvokingJobDetailFactoryBean();
+            jobDetail.setTargetObject(SpringContextHolder.getBean(beanName));
+            jobDetail.setTargetMethod(quartzProperties.getInvokeMethod());
+            jobDetail.setConcurrent(false);
+            jobDetail.afterPropertiesSet();
+
+            // 2. 创建 cronTrigger, 注入 jobDetail
+            CronTriggerFactoryBean cronTrigger = new CronTriggerFactoryBean();
+            cronTrigger.setJobDetail(jobDetail.getObject());
+            cronTrigger.setCronExpression(cronExpression);
+            cronTrigger.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_FIRE_NOW);
+            cronTrigger.afterPropertiesSet();
+
+            return cronTrigger.getObject();
+        } catch (NoSuchMethodException | ParseException | ClassNotFoundException e) {
+            logger.error("创建Trigger错误!", e);
+        }
+        return null;
+    }
+
+}
