53a14b223d0e526b10e9427b25fdaa0f62fe1dee
Ahaochan <844394093@qq.com>
2018-11-21 16:03:41 +0800

    HttpClientHelper 单元测试

---
+++ ahao-commons/src/test/java/com/ahao/commons/http/HttpClientHelperTest.java
@@ -0,0 +1,16 @@
+package com.ahao.commons.http;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HttpClientHelperTest {
+
+    @Test
+    public void connectBaidu() {
+        int code = HttpClientHelper.get("http://www.baidu.com")
+                .execute()
+                .getStatusCode();
+        Assert.assertEquals(200, code);
+    }
+
+}
