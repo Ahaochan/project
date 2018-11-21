574f709b2d6bee6fab8e21a7165aa2e6bb6b8eb1
Ahaochan <844394093@qq.com>
2018-11-21 16:27:59 +0800

    删除无用代码

--- ahao-commons/src/main/java/com/ahao/commons/util/io/ExcelHelper.java
+++ ahao-commons/src/main/java/com/ahao/commons/util/io/ExcelHelper.java
@@ -63,16 +63,4 @@ public class ExcelHelper {
         cell.setCellType(CellType.STRING);
         return cell.getStringCellValue();
     }
-
-
-    public static void main(String[] args) {
-
-        for(int i = 1; i <= 200; i++) {
-            for (int j = 0; j <= 200; j++){
-                if((6*i+17*j)==200) {
-                    System.out.println(i+","+j+","+(6*i+17*j)+","+(6*i)+","+(17*j));
-                }
-            }
-        }
-    }
 }
