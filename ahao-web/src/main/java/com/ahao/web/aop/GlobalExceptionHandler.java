4592371019643a93cf9c3341e0ade1c0ae5b913f
Ahaochan <844394093@qq.com>
2018-11-21 15:45:20 +0800

    全局异常处理

---
+++ ahao-web/src/main/java/com/ahao/web/aop/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.ahao.web.aop;
+
+import com.ahao.core.commons.entity.AjaxDTO;
+import com.ahao.web.exception.AhaoException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 全局的的异常拦截器（拦截所有的控制器）（带有@RequestMapping注解的方法上都会拦截）
+ *
+ * @author fengshuonan
+ * @date 2016年11月12日 下午3:19:56
+ */
+@ControllerAdvice
+@Order(-1)
+public class GlobalExceptionHandler {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 拦截业务异常
+     */
+    @ExceptionHandler(AhaoException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public AjaxDTO ahaoException(AhaoException e) {
+        log.error("业务异常:", e);
+        return AjaxDTO.failure(e.getMessage());
+    }
+
+    /**
+     * 拦截未知的运行时异常
+     */
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public AjaxDTO runtimeException(RuntimeException e) {
+        log.error("运行时异常:", e);
+        return AjaxDTO.failure("服务器异常! 请稍候重试!");
+    }
+}
