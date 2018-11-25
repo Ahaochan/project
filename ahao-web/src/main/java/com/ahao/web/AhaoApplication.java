df6f71fed3c2afd9c63ca5a7c3adb564ea630572
Ahaochan <844394093@qq.com>
2018-11-25 15:36:00 +0800

    添加web启动类

---
+++ ahao-web/src/main/java/com/ahao/web/AhaoApplication.java
@@ -0,0 +1,24 @@
+package com.ahao.web;
+
+import com.ahao.spring.bean.PackageBeanNameGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot方式启动类
+ * @author Ahaochan
+ */
+@SpringBootApplication
+public class AhaoApplication {
+
+    private final static Logger logger = LoggerFactory.getLogger(AhaoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AhaoApplication.class);
+        app.setBeanNameGenerator(new PackageBeanNameGenerator());
+        app.run(args);
+        logger.info(AhaoApplication.class.getSimpleName() + " is success!");
+    }
+}
---
+++ ahao-web/src/main/java/com/ahao/web/AhaoServletInitializer.java
@@ -0,0 +1,18 @@
+package com.ahao.web;
+
+import com.ahao.spring.bean.PackageBeanNameGenerator;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Web程序启动类
+ * @author Ahaochan
+ */
+public class AhaoServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AhaoApplication.class)
+                .beanNameGenerator(new PackageBeanNameGenerator());
+    }
+}
