b8276dc7c79fe17041a350deb8dc18ee50e2ba04
Ahaochan <844394093@qq.com>
2018-12-26 16:32:13 +0800

    注释Feign避免Autowired失败

--- ahao-spring-cloud-eureka/src/main/java/com/ahao/spring/cloud/eureka/Client.java
+++ ahao-spring-cloud-eureka/src/main/java/com/ahao/spring/cloud/eureka/Client.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,20 +60,20 @@ public class Client {
     // ================================ 使用 LoadBalancerClient 做负载均衡  =================================================
 
     // ================================ 使用 Feign 做负载均衡  =================================================
-    // 注意要打开启动类的 @EnableFeignClients 注解
-    @FeignClient("EUREKA") // 由客户端配置文件中的 spring.application.name 配置
-    public interface SimpleFeignClient {
-        // 需要 spring-cloud-starter-feign 依赖
-        @GetMapping("/get") // 由@RequestMapping配置
-        String getTest();
-    }
-    @Autowired
-    private SimpleFeignClient feignClient;
-
-    @GetMapping("/test3")
-    public String test3() {
-        String response = feignClient.getTest();
-        return "Feign请求了: "+response;
-    }
+//    // 注意要打开启动类的 @EnableFeignClients 注解
+//    @FeignClient("EUREKA") // 由客户端配置文件中的 spring.application.name 配置
+//    public interface SimpleFeignClient {
+//        // 需要 spring-cloud-starter-feign 依赖
+//        @GetMapping("/get") // 由@RequestMapping配置
+//        String getTest();
+//    }
+//    @Autowired
+//    private SimpleFeignClient feignClient;
+//
+//    @GetMapping("/test3")
+//    public String test3() {
+//        String response = feignClient.getTest();
+//        return "Feign请求了: "+response;
+//    }
     // ================================ 使用 Feign 做负载均衡  =================================================
 }
