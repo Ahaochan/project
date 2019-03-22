8ce1806111c621c20a4f144623023a522d9a8845
Ahaochan <844394093@qq.com>
2019-03-22 18:44:55 +0800

    限制登录次数 的 Credential 验证器

---
+++ ahao-web/src/main/java/com/ahao/rbac/shiro/credential/RetryLimitHashedCredentialsMatcher.java
@@ -0,0 +1,64 @@
+package com.ahao.rbac.shiro.credential;
+
+import com.ahao.rbac.shiro.entity.ShiroUser;
+import com.ahao.redis.RedisHelper;
+import com.ahao.redis.RedisKeys;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 限制登录次数 的 Credential 验证器
+ * 使用 {@link org.apache.shiro.realm.AuthorizingRealm#setCredentialsMatcher(CredentialsMatcher)} 注入 Realm 即可.
+ */
+public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher {
+    private static final Logger logger = LoggerFactory.getLogger(RetryLimitHashedCredentialsMatcher.class);
+
+    private int maxRetryCount = 3;
+
+    @Override
+    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+        // 1. 根据 principal 获取用户
+        ShiroUser user = new ShiroUser();
+        // 2. 查询获取用户登录次数, 没有则初始化
+        String redisKey = String.format(RedisKeys.SHIRO_RETRY, user.getId());
+        AtomicInteger retryCount = RedisHelper.get(redisKey);
+        if (retryCount == null) {
+            retryCount = new AtomicInteger(0);
+        }
+
+        // 3. 如果用户登陆失败次数大于 maxRetryCount 次, 则 锁定用户并抛出异常
+        if (retryCount.incrementAndGet() > maxRetryCount) {
+            if (user.getLocked() == null || !user.getLocked()){
+                user.setLocked(true);
+                // TODO 更新数据库字段
+            }
+            logger.info("锁定用户{}", user.getId());
+            throw new LockedAccountException("重试次数过多, 该用户已被锁定!");
+        }
+
+        // 4. 正常的凭证判断逻辑, 登陆成功则删除, 否则更新
+        boolean matches = super.doCredentialsMatch(token, info);
+        if (matches) {
+            RedisHelper.del(redisKey);
+        } else {
+            RedisHelper.set(redisKey, retryCount);
+        }
+        return matches;
+    }
+
+    // ================================== Getter And Setter ========================================
+    public int getMaxRetryCount() {
+        return maxRetryCount;
+    }
+
+    public void setMaxRetryCount(int maxRetryCount) {
+        this.maxRetryCount = maxRetryCount;
+    }
+}
---
+++ ahao-web/src/main/java/com/ahao/redis/RedisKeys.java
@@ -0,0 +1,17 @@
+package com.ahao.redis;
+
+/**
+ * Redis Key 存储, 避免 Key 覆盖
+ *
+ * Key 统一以 : 分割, 并根据数据类型以特定字符开头
+ * 1. String: s
+ * 2. Hash: h
+ * 3. List: l
+ * 4. Set: set
+ * 5. Sorted Set: z
+ *
+ * 比如 String 类型, s:user:1:name
+ */
+public interface RedisKeys {
+    String SHIRO_RETRY = "s:shiro:user:%s:retry";
+}
