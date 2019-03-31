5607d93228d7df6ac94fcb9389daf64b1f74d3ce
Ahaochan <844394093@qq.com>
2019-03-31 15:04:40 +0800

    Redis 修复包装类和基本数据类型强转失败的bug

--- ahao-web/src/main/java/com/ahao/redis/util/RedisHelper.java
+++ ahao-web/src/main/java/com/ahao/redis/util/RedisHelper.java
@@ -1,6 +1,7 @@
 package com.ahao.redis.util;
 
 import com.ahao.spring.util.SpringContextHolder;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.Arrays;
@@ -17,8 +18,44 @@ public class RedisHelper {
         return getRedisTemplate().delete(Arrays.asList(keys));
     }
 
-    public static <T> T get(String key) {
-        return (T) getRedisTemplate().opsForValue().get(key);
+    @SuppressWarnings("unchecked")
+    public static <T> T get(String key, Class<T> clazz) {
+        // 1. 处理 void
+        if(clazz == void.class || clazz == Void.class) {
+            return null;
+        }
+
+        // 2. 从 Redis 中获取数据
+        Object obj = getRedisTemplate().opsForValue().get(key);
+        String string = String.valueOf(obj);
+
+        // 3. 处理 char 类型
+        if(clazz == char.class || clazz == Character.class) {
+            return (T) Character.valueOf(string.charAt(0));
+        }
+
+        // 4. 处理数值类型
+        if(NumberUtils.isCreatable(string)) {
+            if(clazz == byte.class || clazz == Byte.class) {
+                return (T) Byte.valueOf(string);
+            }
+            if(clazz == short.class || clazz == Short.class) {
+                return (T) Short.valueOf(string);
+            }
+            if(clazz == int.class || clazz == Integer.class) {
+                return (T) Integer.valueOf(string);
+            }
+            if(clazz == long.class || clazz == Long.class) {
+                return (T) Long.valueOf(string);
+            }
+            if(clazz == float.class || clazz == Float.class) {
+                return (T) Float.valueOf(string);
+            }
+            if(clazz == double.class || clazz == Double.class) {
+                return (T) Double.valueOf(string);
+            }
+        }
+        return (T) obj;
     }
     public static int getInt(String key) {
         Object value = getRedisTemplate().opsForValue().get(key);
