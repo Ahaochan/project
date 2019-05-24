4c0c12322452695e6ab3f45564f70ef3c06d8ac4
Ahaochan <844394093@qq.com>
2019-05-24 17:39:07 +0800

    ServerEndpoint 实现的 WebSocket 服务端

---
+++ ahao-spring-boot-websocket-endpoint/src/main/java/com/ahao/spring/boot/websocket/endpoint/controller/ChatController.java
@@ -0,0 +1,50 @@
+package com.ahao.spring.boot.websocket.endpoint.controller;
+
+import com.ahao.spring.boot.websocket.endpoint.service.ChatService;
+import com.ahao.util.spring.SpringContextHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+@Component
+@ServerEndpoint("/chat")
+public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+//    private static ChatService chatService;
+//    @Autowired
+//    public void setChatService(ChatService chatService) {
+//        ChatController.chatService = chatService;
+////        chatService = SpringContextHolder.getBean(ChatService.class);
+//    }
+
+    public ChatService getChatService() {
+        return SpringContextHolder.getBean(ChatService.class);
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        logger.info("{} : 打开新的Session{}", this.toString(), session.getId()); // 每次创建一个 ChatController 对象
+        getChatService().joinChat(session);
+        getChatService().sendToAll(session.getId()+"加入了群聊");
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) {
+        getChatService().sendToAll(session.getId() + ": " + message);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        getChatService().cancelChat(session);
+        getChatService().sendToAll(session.getId()+"离开了群聊");
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+}
---
+++ ahao-spring-boot-websocket-endpoint/src/main/java/com/ahao/spring/boot/websocket/endpoint/service/ChatService.java
@@ -0,0 +1,12 @@
+package com.ahao.spring.boot.websocket.endpoint.service;
+
+import javax.websocket.Session;
+
+public interface ChatService {
+
+    void joinChat(Session session);
+
+    void cancelChat(Session session);
+
+    void sendToAll(String msg);
+}
