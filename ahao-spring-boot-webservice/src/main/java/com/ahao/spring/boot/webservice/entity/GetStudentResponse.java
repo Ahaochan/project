772b60829a51720178e850c4eea2dfac77020a5e
Ahaochan <844394093@qq.com>
2019-06-03 22:22:28 +0800

    添加实体

---
+++ ahao-spring-boot-webservice/src/main/java/com/ahao/spring/boot/webservice/entity/GetStudentResponse.java
@@ -0,0 +1,67 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.7 生成的
+// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2019.05.25 时间 11:24:24 PM CST 
+//
+
+
+package com.ahao.spring.boot.webservice.entity;
+
+import javax.xml.bind.annotation.*;
+
+
+/**
+ * <p>anonymous complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Student" type="{http://ahao.com/spring/boot/webservice/entity}Student"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "student"
+})
+@XmlRootElement(name = "GetStudentResponse")
+public class GetStudentResponse {
+
+    @XmlElement(name = "Student", required = true)
+    protected Student student;
+
+    /**
+     * 获取student属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link Student }
+     *     
+     */
+    public Student getStudent() {
+        return student;
+    }
+
+    /**
+     * 设置student属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Student }
+     *     
+     */
+    public void setStudent(Student value) {
+        this.student = value;
+    }
+
+}
