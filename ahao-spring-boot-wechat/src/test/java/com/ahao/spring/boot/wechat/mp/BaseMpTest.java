c50f53e2bb6381f0ff27aa64b746483dc6ac24ef
Ahaochan <844394093@qq.com>
2019-06-20 17:50:27 +0800

    添加 临时素材上传 的单元测试

--- ahao-spring-boot-wechat/src/test/java/com/ahao/spring/boot/wechat/mp/BaseMpTest.java
+++ ahao-spring-boot-wechat/src/test/java/com/ahao/spring/boot/wechat/mp/BaseMpTest.java
@@ -1,9 +1,10 @@
 package com.ahao.spring.boot.wechat.mp;
 
 import com.ahao.spring.boot.Starter;
+import com.ahao.util.commons.lang.ReflectHelper;
 import me.chanjar.weixin.common.error.WxErrorException;
-import me.chanjar.weixin.mp.api.WxMpMenuService;
-import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -22,10 +25,21 @@ public class BaseMpTest {
     protected WxMpService wxMpService;
 
     protected WxMpMenuService menuService;
+    protected WxMpKefuService kefuService;
+    protected WxMpUserService userService;
+    protected WxMpMaterialService materialService;
 
     @BeforeEach
     public void init() throws WxErrorException {
         Assumptions.assumeTrue(StringUtils.isNotBlank(wxMpService.getWxMpConfigStorage().getAppId()), "需配置实际的微信配置");
+
+        Map<String, WxMpConfigStorage> configStorageMap = ReflectHelper.getValue(wxMpService, "configStorageMap");
+        configStorageMap.forEach((k,v) -> System.out.println("加载微信公众配置:"+v.toString()));
+        Assertions.assertEquals(1, configStorageMap.size(), "单元测试公众号测试只能配置一个公众号");
+
         menuService = wxMpService.getMenuService();
+        kefuService = wxMpService.getKefuService();
+        userService = wxMpService.getUserService();
+        materialService = wxMpService.getMaterialService();
     }
 }
---
+++ ahao-spring-boot-wechat/src/test/java/com/ahao/spring/boot/wechat/mp/media/MediaTempTest.java
@@ -0,0 +1,54 @@
+package com.ahao.spring.boot.wechat.mp.media;
+
+import com.ahao.spring.boot.wechat.mp.BaseMpTest;
+import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.bean.result.WxMediaUploadResult;
+import me.chanjar.weixin.common.error.WxErrorException;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.File;
+
+class MediaTempTest extends BaseMpTest {
+
+    /**
+     * 上传有效期为 3 天的临时素材
+     * @param mediaFileType 文件类型{@link WxConsts.MediaFileType}
+     * @param filepath      上传文件的所在路径
+     * @see <a href="https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1444738726">新增临时素材</a>
+     */
+    @ParameterizedTest
+    @CsvSource({"image,D:\\123.png"})
+    void upload(String mediaFileType, String filepath) throws WxErrorException {
+        File file = new File(filepath);
+
+//        WxMediaUploadResult result = materialService.mediaUpload(WxConsts.MediaFileType.IMAGE, file);
+        WxMediaUploadResult result = materialService.mediaUpload(mediaFileType, file);
+
+        System.out.println("媒体文件类型: " + result.getType());
+        System.out.println("上传时间戳: " + result.getCreatedAt());
+        System.out.println("媒体文件标识: " + result.getMediaId());
+        System.out.println("视频消息缩略图标识:" + result.getThumbMediaId());
+    }
+
+
+    /**
+     * 获取 {@link #upload(String, String)} 上传的临时素材
+     * @param mediaId  	媒体文件标识
+     * @param filepath  输出文件的路径
+     * @throws WxErrorException
+     * @see <a href="https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1444738727">获取临时素材</a>
+     */
+    @ParameterizedTest
+    @CsvSource({"fbcMU8-_RNb2ins3AwdBIswrzNNkpTKC5YYoWVeY8IpACsWXfM3d6o9tf_WV9mz2,D:\\123.png"})
+    void download(String mediaId, String filepath) throws Exception {
+        File file = materialService.mediaDownload(mediaId);
+
+        FileUtils.copyFile(file, new File(filepath));
+
+        Assertions.assertTrue(file.exists());
+        Assertions.assertTrue(file.length() > 0);
+    }
+}
