e1d9d3cd620bc13b5ed11292f0d7dcc6d94f8170
Ahaochan <844394093@qq.com>
2019-06-25 23:23:13 +0800

    spring cloud 统一配置中心

---
+++ ahao-spring-cloud-config-client/src/main/java/com/ahao/spring/cloud/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.ahao.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Value("${info.description}")
+    private String description;
+
+    @GetMapping("/test")
+    public String test() {
+        return description;
+    }
+}
