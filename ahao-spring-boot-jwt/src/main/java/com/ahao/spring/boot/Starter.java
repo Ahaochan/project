324800bab7c37f2168b9a51a7b0ee046420478a6
Ahaochan <844394093@qq.com>
2019-07-02 17:12:42 +0800

    spring boot 整合 jjwt

---
+++ ahao-spring-boot-jwt/src/main/java/com/ahao/spring/boot/Starter.java
@@ -0,0 +1,24 @@
+package com.ahao.spring.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * SpringBoot方式启动类
+ * @author Ahaochan
+ */
+@SpringBootApplication(scanBasePackages = "com.ahao")
+@EnableConfigurationProperties
+public class Starter {
+
+    private final static Logger logger = LoggerFactory.getLogger(Starter.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Starter.class);
+        app.run(args);
+        logger.info(Starter.class.getSimpleName() + " is success!");
+    }
+}
