279a7dd936deb2dfb6c2701556016a58b74838d7
Ahaochan <844394093@qq.com>
2019-07-03 12:21:33 +0800

    Spring 异步任务

---
+++ ahao-spring-boot-async/src/test/java/com/ahao/spring/boot/async/AsyncTest.java
@@ -0,0 +1,56 @@
+package com.ahao.spring.boot.async;
+
+import com.ahao.spring.boot.Starter;
+import com.ahao.spring.boot.async.service.TestService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.Future;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(classes = Starter.class)
+class AsyncTest {
+    @Autowired
+    private TestService testService;
+
+    @Test
+    void testVoid() throws Exception {
+        String msg = "hello World";
+        testService.executeVoid(msg);
+        Assertions.assertNull(TestService.value);
+
+        Thread.sleep(2000);
+        Assertions.assertEquals(msg, TestService.value);
+    }
+
+    @Test
+    void testFuture() throws Exception {
+        int i = 1;
+        Future<Integer> future = testService.executeFuture(i);
+        Assertions.assertEquals(Integer.valueOf(i + 100), future.get());
+    }
+
+    @Test
+    void testException() throws Exception {
+        // 1. 重定向输出
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream sout = System.out;
+        System.setOut(new PrintStream(baos));
+
+        testService.executeException();
+        Thread.sleep(3000);
+        String msg = baos.toString();
+        System.setOut(sout);
+
+        Assertions.assertEquals("com.ahao.spring.boot.async.service.TestService#executeException(), 错误信息: 错误\r\n", msg);
+
+    }
+}
