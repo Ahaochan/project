cbdbe891f28a57bd7c8b7725124bf34ca1806c6f
Ahaochan <844394093@qq.com>
2019-07-04 12:11:52 +0800

    spring boot admin

---
+++ ahao-spring-boot-admin-server/src/main/java/com/ahao/spring/boot/admin/config/AdminConfig.java
@@ -0,0 +1,9 @@
+package com.ahao.spring.boot.admin.config;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAdminServer
+public class AdminConfig {
+}
