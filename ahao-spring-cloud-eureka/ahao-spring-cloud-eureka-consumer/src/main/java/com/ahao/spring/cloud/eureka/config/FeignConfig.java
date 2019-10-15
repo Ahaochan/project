6f40229e3b457441aae0754bbd3dc501c780382c
Ahaochan <844394093@qq.com>
2019-10-16 00:17:29 +0800

    feign 支持 form data 上传文件

---
+++ ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/config/FeignConfig.java
@@ -0,0 +1,29 @@
+package com.ahao.spring.cloud.eureka.config;
+
+import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+    // new一个form编码器，实现支持form表单提交
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+
+    // 开启Feign的日志
+    @Bean
+    public Logger.Level logger() {
+        return Logger.Level.FULL;
+    }
+}
--- ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/controller/FeignController.java
+++ ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/controller/FeignController.java
@@ -3,26 +3,36 @@ package com.ahao.spring.cloud.eureka.controller;
 import com.ahao.spring.cloud.eureka.EurekaConsumerApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FeignController {
     private static final String serverName = EurekaConsumerApplication.serverName;
     // ================================ 使用 Feign 做负载均衡  =================================================
     // 注意要打开启动类的 @EnableFeignClients 注解
-    @FeignClient(serverName) // 由客户端配置文件中的 spring.application.name 配置
+    @FeignClient(value = serverName) // 由客户端配置文件中的 spring.application.name 配置
     public interface SimpleFeignClient {
         // 需要 spring-cloud-starter-feign 依赖
         @GetMapping("/no-args") // 由@RequestMapping配置
         String noArgs();
+
+        @RequestMapping(value = "/form-data", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        String formData(@RequestParam String filename, @RequestPart("file") MultipartFile file);
     }
     @Autowired
     private SimpleFeignClient feignClient;
 
     @GetMapping("/no-args3")
-    public String test3() {
+    public String noArgs() {
         String response = feignClient.noArgs();
+        return "Feign请求了: "+response;
+    }
+
+    @RequestMapping(value = "/form-data3", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String formData(@RequestParam String filename, @RequestPart("file") MultipartFile file) {
+        String response = feignClient.formData(filename, file);
         return "Feign请求了: "+response;
     }
     // ================================ 使用 Feign 做负载均衡  =================================================
