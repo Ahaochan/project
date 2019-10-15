8091dabf8dcc949e358c8693703143fe65e0d281
Ahaochan <844394093@qq.com>
2019-10-16 00:23:19 +0800

    优化 feign 配置

--- ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/EurekaConsumerApplication.java
+++ ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/EurekaConsumerApplication.java
@@ -2,10 +2,8 @@ package com.ahao.spring.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class EurekaConsumerApplication {
     public static final String serverName = "eureka-provider";
 
