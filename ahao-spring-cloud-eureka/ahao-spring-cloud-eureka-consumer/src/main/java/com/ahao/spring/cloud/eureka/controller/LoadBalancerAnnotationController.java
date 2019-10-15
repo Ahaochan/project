567748444c001da8f1bd82a37fa525480454c069
Ahaochan <844394093@qq.com>
2019-10-15 23:14:21 +0800

    eureka 拆分模块

---
+++ ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-consumer/src/main/java/com/ahao/spring/cloud/eureka/controller/LoadBalancerAnnotationController.java
@@ -0,0 +1,34 @@
+package com.ahao.spring.cloud.eureka.controller;
+
+import com.ahao.spring.cloud.eureka.EurekaConsumerApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 使用 @LoadBalanced 做负载均衡
+ */
+@RestController
+public class LoadBalancerAnnotationController {
+    @Bean
+    @LoadBalanced // 负载均衡配置
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/no-args1")
+    public String test1() {
+        // http://服务名/请求路径
+        // 服务名   由客户端配置文件中的 spring.application.name 配置
+        // 请求路径 由@RequestMapping配置
+        // 跑这个 demo 需要在 application.yml 中把 eureka.client.register-with-eureka 暂时设置为true
+        String serverName = EurekaConsumerApplication.serverName;
+        String response = restTemplate.getForObject("http://"+serverName+"/no-args", String.class);
+        return "RestTemplate请求了"+serverName+": "+response;
+    }
+}
--- ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-server/src/main/java/com/ahao/spring/cloud/eureka/EurekaServerApplication.java
+++ ahao-spring-cloud-eureka/ahao-spring-cloud-eureka-server/src/main/java/com/ahao/spring/cloud/eureka/EurekaServerApplication.java
@@ -6,11 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-//@EnableFeignClients
-public class EurekaApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
