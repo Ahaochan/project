04c01f215bc38591e1167cd1caf9c08782fa5f7e
Ahaochan <844394093@qq.com>
2019-11-05 10:41:08 +0800

    fix bug 修复没有ack的bug

--- ahao-spring-boot-rabbitmq/src/test/java/com/ahao/spring/boot/rabbitmq/DirectConsumer.java
+++ ahao-spring-boot-rabbitmq/src/test/java/com/ahao/spring/boot/rabbitmq/DirectConsumer.java
@@ -2,9 +2,12 @@ package com.ahao.spring.boot.rabbitmq;
 
 import com.ahao.domain.entity.AjaxDTO;
 import com.ahao.domain.entity.BaseDO;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -21,40 +24,67 @@ public class DirectConsumer {
 
     public static Object value;
 
-
     @RabbitHandler
-    public void directQueue(@Payload String msg) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("消息接收时间:"+sdf.format(new Date()));
-        System.out.println("接收到的消息:"+msg);
-        Thread.sleep(1000);
-        value = msg;
+    public void directQueue(@Payload String msg, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws Exception {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("消息接收时间:" + sdf.format(new Date()));
+            System.out.println("接收到的消息:" + msg);
+            Thread.sleep(1000);
+            value = msg;
+
+            channel.basicAck(tag, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(tag, false, false);
+        }
     }
 
     @RabbitHandler
-    public void directQueue(@Payload Integer msg) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("消息接收时间:"+sdf.format(new Date()));
-        System.out.println("接收到的消息:"+msg);
-        Thread.sleep(1000);
-        value = msg;
+    public void directQueue(@Payload Integer msg, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws Exception {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("消息接收时间:" + sdf.format(new Date()));
+            System.out.println("接收到的消息:" + msg);
+            Thread.sleep(1000);
+            value = msg;
+
+            channel.basicAck(tag, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(tag, false, false);
+        }
     }
 
     @RabbitHandler
-    public void directQueue(@Payload AjaxDTO msg) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("消息接收时间:"+sdf.format(new Date()));
-        System.out.println("接收到的消息:"+msg.getResult() +"," + msg.getMsg() +"," +msg.getObj());
-        Thread.sleep(1000);
-        value = msg;
+    public void directQueue(@Payload AjaxDTO msg, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws Exception {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("消息接收时间:" + sdf.format(new Date()));
+            System.out.println("接收到的消息:" + msg.getResult() + "," + msg.getMsg() + "," + msg.getObj());
+            Thread.sleep(1000);
+            value = msg;
+
+            channel.basicAck(tag, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(tag, false, false);
+        }
     }
 
     @RabbitHandler
-    public void directQueue(@Payload BaseDO msg) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("消息接收时间:"+sdf.format(new Date()));
-        System.out.println("接收到的消息:"+msg.toString());
-        Thread.sleep(1000);
-        value = msg;
+    public void directQueue(@Payload BaseDO msg, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws Exception {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.println("消息接收时间:" + sdf.format(new Date()));
+            System.out.println("接收到的消息:" + msg.toString());
+            Thread.sleep(1000);
+            value = msg;
+
+            channel.basicAck(tag, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(tag, false, false);
+        }
     }
 }
