cf843e0daea0b1fea81f868060aecf58e09a08e7
Ahaochan <844394093@qq.com>
2019-11-15 18:28:22 +0800

    完善 jpa 测试用例

--- ahao-spring-boot-jpa/src/test/java/moe/ahao/spring/boot/jpa/SampleTest.java
+++ ahao-spring-boot-jpa/src/test/java/moe/ahao/spring/boot/jpa/SampleTest.java
@@ -1,8 +1,10 @@
 package moe.ahao.spring.boot.jpa;
 
+import com.ahao.util.commons.io.JSONHelper;
 import moe.ahao.spring.boot.Starter;
 import moe.ahao.spring.boot.jpa.module.entity.User;
 import moe.ahao.spring.boot.jpa.module.mapper.UserMapper;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
@@ -25,15 +28,31 @@ class SampleTest {
     private UserMapper userMapper;
 
     @Test
-    void testNativeSql() {
+    void testNativeSql() throws Exception {
         int count = userMapper.getCount();
         Assertions.assertEquals(5, count);
+
+        Date date = DateUtils.parseDate("2019-11-13 12:00:00", "yyyy-MM-dd HH:mm:ss");
+        List<User> list = userMapper.findByCreateTimeAfter(date);
+        Assertions.assertEquals(2, list.size());
+        System.out.println(JSONHelper.toString(list));
     }
 
     @Test
     void testLikeQuery() {
         List<User> leftLikeList = userMapper.getUsersByUsernameLike("user%");
         Assertions.assertEquals(5, leftLikeList.size());
+
+        List<User> allLikeList = userMapper.findByNameAllLike("user");
+        Assertions.assertEquals(5, allLikeList.size());
+    }
+
+    @Test
+    void testHql() throws Exception {
+        Date start = DateUtils.parseDate("2019-11-12 12:00:00", "yyyy-MM-dd HH:mm:ss");
+        Date end = DateUtils.parseDate("2019-11-14 12:00:00", "yyyy-MM-dd HH:mm:ss");
+        List<User> between = userMapper.findByCreateTimeBetween(start, end);
+        Assertions.assertEquals(2, between.size());
     }
 
     @Test
--- ahao-spring-boot-jpa/src/test/resources/sql/DML.sql
+++ ahao-spring-boot-jpa/src/test/resources/sql/DML.sql
@@ -2,8 +2,8 @@ TRUNCATE TABLE user;
 
 INSERT INTO user (id, username, email, password, salt, sex, create_by, update_by, create_time, update_time)
 VALUES
-(1, 'user1', '1@qq.com', 'pw1', 'salt1', 1, -1, -1, now(), now()),
-(2, 'user2', '2@qq.com', 'pw2', 'salt2', 1, -1, -1, now(), now()),
-(3, 'user3', '3@qq.com', 'pw3', 'salt3', 2, -1, -1, now(), now()),
-(4, 'user4', '4@qq.com', 'pw4', 'salt4', 2, -1, -1, now(), now()),
-(5, 'user5', '5@qq.com', 'pw5', 'salt5', 2, -1, -1, now(), now());
+(1, 'user1', '1@qq.com', 'pw1', 'salt1', 1, -1, -1, '2019-11-11 00:00:00', now()),
+(2, 'user2', '2@qq.com', 'pw2', 'salt2', 1, -1, -1, '2019-11-12 00:00:00', now()),
+(3, 'user3', '3@qq.com', 'pw3', 'salt3', 2, -1, -1, '2019-11-13 00:00:00', now()),
+(4, 'user4', '4@qq.com', 'pw4', 'salt4', 2, -1, -1, '2019-11-14 00:00:00', now()),
+(5, 'user5', '5@qq.com', 'pw5', 'salt5', 2, -1, -1, '2019-11-15 00:00:00', now());
