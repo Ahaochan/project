6605b00138f9d893aa46a4efc189ba1954f10908
Ahaochan <844394093@qq.com>
2020-04-11 20:28:50 +0800

    init unit test transactionMessage

---
+++ ahao-spring-boot-rocketmq/src/test/java/moe/ahao/spring/boot/rocketmq/MyRocketMQTransactionListener.java
@@ -0,0 +1,54 @@
+package moe.ahao.spring.boot.rocketmq;
+
+import org.apache.rocketmq.spring.annotation.RocketMQTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
+import org.apache.rocketmq.spring.core.RocketMQLocalTransactionState;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RocketMQTransactionListener(txProducerGroup = ProducerTest.GROUP_TX)
+public class MyRocketMQTransactionListener implements RocketMQLocalTransactionListener {
+    public List<String> receiveList = new ArrayList<>();
+
+    @Override
+    public RocketMQLocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+        MessageHeaders headers = msg.getHeaders();
+        String transactionId = headers.get(RocketMQHeaders.TRANSACTION_ID, String.class);
+        String payload = msg.getPayload().toString();
+
+        try {
+            if(receiveList.size() > 5) {
+                return RocketMQLocalTransactionState.UNKNOWN; // 模拟异常
+            }
+            receiveList.add(payload);
+            System.out.println("本地事务消息 transactionId:" + transactionId + ", payload:" + payload);
+            return RocketMQLocalTransactionState.COMMIT;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RocketMQLocalTransactionState.ROLLBACK;
+        }
+    }
+
+    @Override
+    public RocketMQLocalTransactionState checkLocalTransaction(Message msg) {
+        MessageHeaders headers = msg.getHeaders();
+        String transactionId = headers.get(RocketMQHeaders.TRANSACTION_ID, String.class);
+        String payload = msg.getPayload().toString();
+
+        try {
+            System.out.println("本地事务消息, 异常回查: transactionId:" + transactionId + ", payload:" + payload);
+            if(receiveList.contains(payload)) {
+                return RocketMQLocalTransactionState.COMMIT;
+            } else {
+                return RocketMQLocalTransactionState.ROLLBACK;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return RocketMQLocalTransactionState.ROLLBACK;
+        }
+    }
+}
--- ahao-spring-boot-rocketmq/src/test/java/moe/ahao/spring/boot/rocketmq/ProducerTest.java
+++ ahao-spring-boot-rocketmq/src/test/java/moe/ahao/spring/boot/rocketmq/ProducerTest.java
@@ -2,22 +2,29 @@ package moe.ahao.spring.boot.rocketmq;
 
 import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Comparator;
+import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ContextConfiguration(classes = {RocketMQAutoConfiguration.class, MyRocketMQListener.class})
+@ContextConfiguration(classes = {RocketMQAutoConfiguration.class,
+    MyRocketMQListener.class, MyRocketMQTransactionListener.class})
 @ActiveProfiles("rocketmq")
 public class ProducerTest {
+    public static final String GROUP_TX = "ahao-transaction-group";
     public static final String TOPIC  = "ahao-topic";
 
     @Autowired
@@ -27,7 +34,7 @@ public class ProducerTest {
     private MyRocketMQListener listener;
 
     @Test
-    public void simpleMessage() throws InterruptedException {
+    public void simpleMessage() throws Exception {
         for (int i = 0; i < 10; i++) {
             rocketMQTemplate.convertAndSend(TOPIC, "payload-"+i);
         }
@@ -37,5 +44,26 @@ public class ProducerTest {
         for (int i = 0; i < 10; i++) {
             Assertions.assertEquals("payload-"+i, listener.receiveList.get(i));
         }
+    }
+
+    @Test
+    public void transactionMessage() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            Message message = MessageBuilder.withPayload("payload-"+i)
+                .setHeader(RocketMQHeaders.TRANSACTION_ID, UUID.randomUUID().toString())
+                .build();
+            this.rocketMQTemplate.sendMessageInTransaction(GROUP_TX, TOPIC, message, null);
+        }
+        Thread.sleep(5000);
+
+        listener.receiveList.sort(Comparator.comparing(String::toString));
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals("payload-"+i, listener.receiveList.get(i));
+        }
+    }
+
+    @AfterEach
+    public void after() {
+        listener.receiveList.clear();
     }
 }
