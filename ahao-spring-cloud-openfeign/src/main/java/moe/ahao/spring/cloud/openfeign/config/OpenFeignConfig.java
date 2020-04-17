73eb813bd11187a68ba2f6aa600b6b9e58109ce6
Ahaochan <844394093@qq.com>
2020-04-17 20:51:53 +0800

    自定义feignDecoder

--- ahao-spring-cloud-openfeign/src/main/java/moe/ahao/spring/cloud/openfeign/config/OpenFeignConfig.java
+++ ahao-spring-cloud-openfeign/src/main/java/moe/ahao/spring/cloud/openfeign/config/OpenFeignConfig.java
@@ -1,9 +1,23 @@
 package moe.ahao.spring.cloud.openfeign.config;
 
+import feign.codec.Decoder;
+import feign.optionals.OptionalDecoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients
 public class OpenFeignConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Decoder feignDecoder(ObjectFactory<HttpMessageConverters> messageConverters) {
+        return new OptionalDecoder(new ResponseEntityDecoder(new SpringDecoder(messageConverters)));
+    }
 }
