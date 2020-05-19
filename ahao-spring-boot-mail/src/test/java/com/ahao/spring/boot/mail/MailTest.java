cf04d826fefdfe0a77c2a73e5a44f4b58158d539
Ahaochan <844394093@qq.com>
2020-05-19 16:19:37 +0800

    减少依赖

--- ahao-spring-boot-mail/src/test/java/com/ahao/spring/boot/mail/MailTest.java
+++ ahao-spring-boot-mail/src/test/java/com/ahao/spring/boot/mail/MailTest.java
@@ -1,11 +1,13 @@
 package com.ahao.spring.boot.mail;
 
-import com.ahao.spring.boot.Starter;
+import com.ahao.util.spring.SpringContextHolder;
 import com.ahao.util.spring.mail.MailHelper;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderValidatorAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -20,14 +22,15 @@ import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ContextConfiguration(classes = Starter.class)
-class MailTest {
+@ContextConfiguration(classes = {SpringContextHolder.class,
+    MailSenderAutoConfiguration.class, MailSenderValidatorAutoConfiguration.class})
+public class MailTest {
 
     @Autowired
     private JavaMailSender mailSender;
 
     @BeforeEach
-    void init() {
+    public void init() {
         String username = MailHelper.getProperties().getUsername();
         Assumptions.assumeTrue(StringUtils.isNotBlank(username), "需要配置实际的邮箱配置");
         Assertions.assertNotNull(mailSender);
@@ -35,7 +38,7 @@ class MailTest {
 
     @Test
     @DisplayName("发送纯文本邮件")
-    void sendSimpleMail() throws Exception {
+    public void sendSimpleMail() throws Exception {
         MailHelper.setSubject("主题: 简单邮件")
             .setContent("测试邮件内容")
             .addToEmails(Arrays.asList("844394093@qq.com"))
@@ -45,7 +48,7 @@ class MailTest {
 
     @Test
     @DisplayName("发送有附件的邮件")
-    void sendAttachmentsMail() throws Exception {
+    public void sendAttachmentsMail() throws Exception {
         Resource file = new ByteArrayResource("hello world".getBytes(StandardCharsets.UTF_8));
         Map<String, Resource> files = new HashMap<>();
         files.put("附件-1.txt", file);
@@ -61,7 +64,7 @@ class MailTest {
 
     @Test
     @DisplayName("发送Html的邮件")
-    void sendHtmlMail() throws Exception {
+    public void sendHtmlMail() throws Exception {
         MailHelper.setSubject("主题: Html")
             .setContent("<html><body><h1>Hello world</h1></body></html>", true)
             .addToEmails(Arrays.asList("844394093@qq.com"))
