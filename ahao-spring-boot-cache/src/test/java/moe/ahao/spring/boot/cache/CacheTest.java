9cc81edd93442c9568da13f83fbf05993b663c70
Ahaochan <844394093@qq.com>
2020-05-19 16:04:41 +0800

    减少依赖

--- ahao-spring-boot-cache/src/test/java/moe/ahao/spring/boot/cache/CacheTest.java
+++ ahao-spring-boot-cache/src/test/java/moe/ahao/spring/boot/cache/CacheTest.java
@@ -1,10 +1,10 @@
 package moe.ahao.spring.boot.cache;
 
-import moe.ahao.spring.boot.Starter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,7 +15,8 @@ import java.util.UUID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ContextConfiguration(classes = Starter.class)
+@ContextConfiguration(classes = {CacheConfig.class, CaffeineCacheConfig.class, RedisCacheConfig.class, TestCacheService.class,
+    RedisAutoConfiguration.class})
 @ActiveProfiles("simple")
 class CacheTest {
 
