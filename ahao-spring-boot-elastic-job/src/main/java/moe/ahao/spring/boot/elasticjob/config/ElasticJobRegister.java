8289baa5051ed58774e9d6d104315f7a579701b5
Ahaochan <844394093@qq.com>
2020-07-18 16:28:06 +0800

    优化 registerSpringJobSchedulerV2 方法, 不从 SpringContextHolder 再次获取 bean

--- ahao-spring-boot-elastic-job/src/main/java/moe/ahao/spring/boot/elasticjob/config/ElasticJobRegister.java
+++ ahao-spring-boot-elastic-job/src/main/java/moe/ahao/spring/boot/elasticjob/config/ElasticJobRegister.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
@@ -29,8 +30,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import javax.sql.DataSource;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 public class ElasticJobRegister implements InitializingBean, ApplicationContextAware {
@@ -68,10 +67,10 @@ public class ElasticJobRegister implements InitializingBean, ApplicationContextA
         JobTypeConfiguration typeConfig = jobProperties.generateJobTypeConfig(coreConfig, clazz);
         LiteJobConfiguration liteConfig = jobProperties.generateLiteJobConfig(defaultProperties, typeConfig);
 
-        registerSpringJobScheduler(defaultProperties, jobProperties, liteConfig, elasticJob);
+        registerSpringJobSchedulerV2(defaultProperties, jobProperties, liteConfig, elasticJob);
     }
 
-    private void registerSpringJobScheduler(DefaultJobProperties defaultProperties, BaseJobProperties jobProperties,
+    private void registerSpringJobSchedulerV1(DefaultJobProperties defaultProperties, BaseJobProperties jobProperties,
                      LiteJobConfiguration liteConfig, ElasticJob job) {
         // 1. 创建 SpringJobScheduler
         BeanDefinitionBuilder factory = BeanDefinitionBuilder.rootBeanDefinition(SpringJobScheduler.class);
@@ -81,6 +80,8 @@ public class ElasticJobRegister implements InitializingBean, ApplicationContextA
         factory.addConstructorArgValue(job);
         factory.addConstructorArgValue(zk);
         factory.addConstructorArgValue(liteConfig);
+        JobEventRdbConfiguration jobEventRdbConfiguration = null;
+        ElasticJobListener[] listeners = new ElasticJobListener[0];
         if (defaultProperties.isJobEventTraceEnabled() || Objects.equals(jobProperties.getJobEventTraceEnabled(), true)) {
             if(!ClassHelper.isSubClass(job, JobEventTraceDataSourceCapable.class)) {
                 throw new ClassCastException(job.getClass().getName() + "未实现 JobEventTraceDataSourceCapable 接口");
@@ -90,35 +91,67 @@ public class ElasticJobRegister implements InitializingBean, ApplicationContextA
             if(dataSource == null) {
                 throw new IllegalArgumentException("事件追踪数据源为空! http://elasticjob.io/docs/elastic-job-lite/02-guide/event-trace/");
             }
-            BeanDefinitionBuilder rdbFactory = BeanDefinitionBuilder.rootBeanDefinition(JobEventRdbConfiguration.class);
-            rdbFactory.addConstructorArgValue(dataSource);
-            factory.addConstructorArgValue(rdbFactory.getBeanDefinition());
+            jobEventRdbConfiguration = new JobEventRdbConfiguration(dataSource);
+            factory.addConstructorArgValue(jobEventRdbConfiguration);
         }
         if(ClassHelper.isSubClass(job, ElasticJobListenerCapable.class)) {
             ElasticJobListenerCapable capable = (ElasticJobListenerCapable) job;
-            List<ElasticJobListener> listeners = capable.getElasticJobListeners();
-            factory.addConstructorArgValue(listeners);
-        } else {
-            factory.addConstructorArgValue(Collections.emptyList());
+            listeners = capable.getElasticJobListeners().toArray(new ElasticJobListener[0]);
         }
+        factory.addConstructorArgValue(listeners);
 
         // 3. 注册到 Spring Application Context
         String jobName = jobProperties.getJobName();
         String beanName = jobName + "SpringJobScheduler";
-        this.registerBean(beanName, factory.getBeanDefinition());
+        AbstractBeanDefinition beanDefinition = factory.getBeanDefinition();
+        BeanDefinitionRegistry registry = (BeanDefinitionRegistry) ctx.getAutowireCapableBeanFactory();
+        if(StringUtils.isBlank(beanName)) {
+            BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinition, registry);
+        } else {
+            registry.registerBeanDefinition(beanName, beanDefinition);
+        }
 
         // 4. 启动定时任务
         SpringJobScheduler springJobScheduler = SpringContextHolder.getBean(beanName);
         springJobScheduler.init();
     }
 
-    private void registerBean(String beanName, AbstractBeanDefinition beanDefinition) {
-        BeanDefinitionRegistry registry = (BeanDefinitionRegistry) ctx.getAutowireCapableBeanFactory();
-
-        if(StringUtils.isBlank(beanName)) {
-            BeanDefinitionReaderUtils.registerWithGeneratedName(beanDefinition, registry);
-        } else {
-            registry.registerBeanDefinition(beanName, beanDefinition);
+    private void registerSpringJobSchedulerV2(DefaultJobProperties defaultProperties, BaseJobProperties jobProperties,
+                                              LiteJobConfiguration liteConfig, ElasticJob job) {
+        // 1. 传入构造器所需参数
+        JobEventRdbConfiguration jobEventRdbConfiguration = null;
+        ElasticJobListener[] listeners = new ElasticJobListener[0];
+        if (defaultProperties.isJobEventTraceEnabled() || Objects.equals(jobProperties.getJobEventTraceEnabled(), true)) {
+            if(!ClassHelper.isSubClass(job, JobEventTraceDataSourceCapable.class)) {
+                throw new ClassCastException(job.getClass().getName() + "未实现 JobEventTraceDataSourceCapable 接口");
+            }
+            JobEventTraceDataSourceCapable capable = (JobEventTraceDataSourceCapable) job;
+            DataSource dataSource = capable.getJobEventTraceDataSource();
+            if(dataSource == null) {
+                throw new IllegalArgumentException("事件追踪数据源为空! http://elasticjob.io/docs/elastic-job-lite/02-guide/event-trace/");
+            }
+            jobEventRdbConfiguration = new JobEventRdbConfiguration(dataSource);
         }
+        if(ClassHelper.isSubClass(job, ElasticJobListenerCapable.class)) {
+            ElasticJobListenerCapable capable = (ElasticJobListenerCapable) job;
+            listeners = capable.getElasticJobListeners().toArray(new ElasticJobListener[0]);
+        }
+
+        // 2. 创建 SpringJobScheduler
+        SpringJobScheduler scheduler;
+        if(jobEventRdbConfiguration == null) {
+            scheduler = new SpringJobScheduler(job, zk, liteConfig, listeners);
+        } else {
+            scheduler = new SpringJobScheduler(job, zk, liteConfig, jobEventRdbConfiguration, listeners);
+        }
+
+        // 3. 注册到 Spring Application Context
+        String jobName = jobProperties.getJobName();
+        String beanName = jobName + "SpringJobScheduler";
+        ConfigurableListableBeanFactory registry = (ConfigurableListableBeanFactory) ctx.getAutowireCapableBeanFactory();
+        registry.registerSingleton(beanName, scheduler);
+
+        // 4. 启动定时任务
+        scheduler.init();
     }
 }
