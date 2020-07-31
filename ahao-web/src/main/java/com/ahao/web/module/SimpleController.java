9940354884053458fff1370fd4922e4f8f35edea
Ahaochan <844394093@qq.com>
2020-07-31 18:04:07 +0800

    add form data 请求方式

--- ahao-web/src/main/java/com/ahao/web/module/SimpleController.java
+++ ahao-web/src/main/java/com/ahao/web/module/SimpleController.java
@@ -26,12 +26,10 @@ public class SimpleController {
     public AjaxDTO get1(Integer result, String msg) {
         return AjaxDTO.get(result, msg, null);
     }
-
     @GetMapping("/get2")
     public AjaxDTO get2(@RequestParam Integer result, @RequestParam String msg) {
         return AjaxDTO.get(result, msg, null);
     }
-
     @GetMapping("/get3")
     public AjaxDTO get3(AjaxDTO req) {
         return req;
@@ -41,43 +39,35 @@ public class SimpleController {
     public AjaxDTO post1(Integer result, String msg) {
         return AjaxDTO.get(result, msg, null);
     }
-
     @PostMapping("/post2")
     public AjaxDTO post2(@RequestParam Integer result, @RequestParam String msg) {
         return AjaxDTO.get(result, msg, null);
     }
-
     @PostMapping("/post3")
     public AjaxDTO post3(AjaxDTO req) {
         return req;
     }
-
     @PostMapping("/post4")
     public AjaxDTO post4(@RequestParam String msg, @RequestBody AjaxDTO req) {
         return AjaxDTO.get(req.getResult(), req.getMsg() + msg, req.getObj());
     }
-
     @PostMapping("/post5")
     public AjaxDTO post5(@RequestBody AjaxDTO req) {
         return req;
     }
 
-
     @PostMapping("multipart1")
     public AjaxDTO multipart1(MultipartFile file) throws IOException {
         return AjaxDTO.success(new String(file.getBytes(), StandardCharsets.UTF_8));
     }
-
     @PostMapping("multipart2")
     public AjaxDTO multipart2(@RequestParam MultipartFile file) throws IOException {
         return AjaxDTO.success(new String(file.getBytes(), StandardCharsets.UTF_8));
     }
-
     @PostMapping("multipart3")
     public AjaxDTO multipart3(@RequestPart MultipartFile file) throws IOException {
         return AjaxDTO.success(new String(file.getBytes(), StandardCharsets.UTF_8));
     }
-
     @PostMapping("multipart4")
     public AjaxDTO multipart4(AjaxDTO req, @RequestParam MultipartFile file) throws IOException {
         return AjaxDTO.success(req.getMsg() + new String(file.getBytes(), StandardCharsets.UTF_8));
@@ -89,5 +79,22 @@ public class SimpleController {
     @PostMapping("multipart6")
     public AjaxDTO multipart6(@RequestPart AjaxDTO req, @RequestPart MultipartFile file) throws IOException {
         return AjaxDTO.success(req.getMsg() + new String(file.getBytes(), StandardCharsets.UTF_8));
+    }
+
+    @PostMapping("form-data1")
+    public AjaxDTO formData1(AjaxDTO req) {
+        return req;
+    }
+    @PostMapping("form-data2")
+    public AjaxDTO formData3(@ModelAttribute AjaxDTO req) {
+        return req;
+    }
+    @PostMapping("form-data3")
+    public AjaxDTO formData3(Integer result, String msg) {
+        return AjaxDTO.get(result, msg, null);
+    }
+    @PostMapping("form-data4")
+    public AjaxDTO formData4(@RequestParam Integer result, @RequestParam String msg) {
+        return AjaxDTO.get(result, msg, null);
     }
 }
