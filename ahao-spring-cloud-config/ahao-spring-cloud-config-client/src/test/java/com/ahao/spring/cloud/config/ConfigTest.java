d7add94c05fd26f0b115a4751601746410de5ada
Ahaochan <844394093@qq.com>
2021-02-02 10:40:27 +0800

    🚚 迁移 spring cloud config 位置

--- ahao-spring-cloud-config/ahao-spring-cloud-config-client/src/test/java/com/ahao/spring/cloud/config/ConfigTest.java
+++ ahao-spring-cloud-config/ahao-spring-cloud-config-client/src/test/java/com/ahao/spring/cloud/config/ConfigTest.java
@@ -1,29 +1,33 @@
 package com.ahao.spring.cloud.config;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-//@ContextConfiguration(classes = Starter.class)
-//@ActiveProfiles("test")
+// @ExtendWith(SpringExtension.class)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+// @ContextConfiguration(classes = Starter.class)
+// @ActiveProfiles("test")
 class ConfigTest {
 
     private MockMvc mockMvc;
 
-//    @Autowired
-//    private WebApplicationContext wac;
-//
-//    @BeforeEach
-//    void init() {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();  //初始化MockMvc对象
-//    }
+   @Autowired
+   private WebApplicationContext wac;
+
+   @BeforeEach
+   void init() {
+       mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();  //初始化MockMvc对象
+   }
 
     @Test
     @Disabled("按照Readme.md文档进行测试")
