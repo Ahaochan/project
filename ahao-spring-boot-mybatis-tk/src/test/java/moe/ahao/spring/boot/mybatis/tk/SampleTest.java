97600ab991b34d7f19a1add6c2484f9541ac8aec
Ahaochan <844394093@qq.com>
2021-02-07 15:24:34 +0800

    ✅ 添加 Mybatis Cursor 应用的单元测试

--- ahao-spring-boot-mybatis-tk/src/test/java/moe/ahao/spring/boot/mybatis/tk/SampleTest.java
+++ ahao-spring-boot-mybatis-tk/src/test/java/moe/ahao/spring/boot/mybatis/tk/SampleTest.java
@@ -3,28 +3,34 @@ package moe.ahao.spring.boot.mybatis.tk;
 import moe.ahao.spring.boot.mybatis.tk.config.MapperConig;
 import moe.ahao.spring.boot.mybatis.tk.module.entity.User;
 import moe.ahao.spring.boot.mybatis.tk.module.mapper.UserMapper;
+import org.apache.ibatis.cursor.Cursor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 import tk.mybatis.mapper.weekend.WeekendSqls;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ContextConfiguration(classes = {MapperConig.class,
-    DataSourceAutoConfiguration.class,
-    MybatisAutoConfiguration.class})
+    DataSourceAutoConfiguration.class, MybatisAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class, TransactionAutoConfiguration.class})
 @ActiveProfiles("test")
-public class SampleTest {
+class SampleTest {
 
     @Autowired
     private UserMapper userMapper;
@@ -75,5 +81,21 @@ public class SampleTest {
         User user2 = userMapper.selectByPrimaryKey(user1.getId());
         Assertions.assertEquals(user1.getUsername(), user2.getUsername());
         Assertions.assertEquals(user1.getPassword(), user2.getPassword());
+    }
+
+    @Test
+    @Transactional
+    void scanFoo0() throws Exception {
+        try (Cursor<User> cursor = userMapper.selectCursorAll()) {
+            Iterator<User> it = cursor.iterator();
+
+            while (it.hasNext()) {
+                List<User> userList = new ArrayList<>();
+                for (int i = 0; i < 3 && it.hasNext(); i++) {
+                    userList.add(it.next());
+                }
+                System.out.println("读取" + userList.size() + "条记录, " + userList);
+            }
+        }
     }
 }
--- ahao-spring-boot-mybatis-tk/src/test/java/moe/ahao/spring/boot/mybatis/tk/module/mapper/UserMapper.java
+++ ahao-spring-boot-mybatis-tk/src/test/java/moe/ahao/spring/boot/mybatis/tk/module/mapper/UserMapper.java
@@ -3,6 +3,10 @@ package moe.ahao.spring.boot.mybatis.tk.module.mapper;
 
 import moe.ahao.spring.boot.mybatis.tk.CommonMapper;
 import moe.ahao.spring.boot.mybatis.tk.module.entity.User;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.cursor.Cursor;
 
 public interface UserMapper extends CommonMapper<User> {
+    @Select("select * from user;")
+    Cursor<User> selectCursorAll();
 }
