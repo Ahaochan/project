efc46eff84f7ee673193bb08cf52bb2f71491caa
Ahaochan <844394093@qq.com>
2021-05-08 01:49:44 +0800

    🐛 修复redis部署脚本

--- ahao-spring-boot-redis/script/install.sh
+++ ahao-spring-boot-redis/script/install.sh
@@ -10,20 +10,36 @@ cd redis-${VERSION}
 make
 
 # 3. 开机时启动
-PORT=6379
+PORT=16379
 cp utils/redis_init_script /etc/init.d/redis_${PORT}
 sed -i "s#REDISPORT=[[:digit:]]*#REDISPORT=${PORT}#g" /etc/init.d/redis_${PORT}
 
 # 4. 配置文件
 mkdir -p /etc/redis /var/redis/${PORT}
 cp redis.conf /etc/redis/${PORT}.conf
-sed -i "s#daemonize no#daemonize yes#g" /etc/redis/${PORT}.conf
-sed -i "s#pidfile /var/run/redis_[[:digit:]]*.pid#pidfile /var/run/redis_${PORT}.pid#g" /etc/redis/${PORT}.conf
-sed -i "s#port [[:digit:]]*#port ${PORT}#g" /etc/redis/${PORT}.conf
+sed -i "s#^daemonize no#daemonize yes#g" /etc/redis/${PORT}.conf
+sed -i "s#^pidfile /var/run/redis_[[:digit:]]*.pid#pidfile /var/run/redis_${PORT}.pid#g" /etc/redis/${PORT}.conf
+sed -i "s#^port [[:digit:]]*#port ${PORT}#g" /etc/redis/${PORT}.conf
 sed -i "s#^dir .*#dir /var/redis/${PORT}#g" /etc/redis/${PORT}.conf
 
+# 5. 主从配置
+PW="password"
+if [ true ]; then
+    # slave节点配置
+    MASTER_HOST="127.0.0.1 6379"
+    sed -i "s/^[# ]*replicaof .*/replicaof ${MASTER_HOST}/g" /etc/redis/${PORT}.conf
+    sed -i "s/^replica-read-only .*/replica-read-only yes/g" /etc/redis/${PORT}.conf
+    sed -i "s/^[# ]*masterauth .*/masterauth ${PW}/g" /etc/redis/${PORT}.conf
+else
+    # master配置
+    sed -i "s/^[# ]*requirepass .*/requirepass ${PW}/g" /etc/redis/${PORT}.conf
+fi;
+
 # 5. 启动脚本
-/etc/redis/redis_${PORT} start
+/etc/init.d/redis_${PORT} start
 
 # 6. 连接
-redis-cli -h 127.0.0.1 -p ${PORT}
+redis-cli -h 127.0.0.1 -p ${PORT} -a ${PW}
+
+# 7. 删除产生的文件
+rm -rf /etc/init.d/redis_${PORT} /etc/redis/${PORT}.conf /var/redis/${PORT} /var/run/redis_${PORT}.pid
