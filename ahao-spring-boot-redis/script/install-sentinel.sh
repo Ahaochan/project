5de6084a2441d75ad11ff310747859f98fd1be79
Ahaochan <844394093@qq.com>
2021-05-11 00:48:28 +0800

    🔧 redis sentinel config

---
+++ ahao-spring-boot-redis/script/install-sentinel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# 1. 安裝依赖
+apt-get install tcl
+
+# 2. 安装 Redis
+VERSION=6.2.2
+wget https://download.redis.io/releases/redis-${VERSION}.tar.gz
+tar xzf redis-${VERSION}.tar.gz
+cd redis-${VERSION}
+make && make test && make install
+
+# 3. 开机时启动
+PORT=26379
+
+# 4. 配置文件
+mkdir -p /etc/sentinel /var/sentinel/${PORT} /var/log/sentinel/${PORT}
+cp sentinel.conf /etc/sentinel/${PORT}.conf
+sed -i "s#^sentinel monitor mymaster .*#sentinel monitor mymaster 127.0.0.1 6379 2#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^sentinel down-after-milliseconds mymaster [[:digit:]]*#sentinel down-after-milliseconds mymaster 30001#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^sentinel parallel-syncs mymaster [[:digit:]]*#sentinel parallel-syncs mymaster 1#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^sentinel failover-timeout mymaster [[:digit:]]*#sentinel failover-timeout mymaster 180000#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^[# ]*bind .*#bind 127.0.0.1#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^port [[:digit:]]*#port ${PORT}#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^daemonize no#daemonize yes#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^pidfile /var/run/redis-sentinel[[:digit:]]*.pid#pidfile /var/run/redis-sentinel${PORT}.pid#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^logfile .*#/var/log/sentinel/${PORT}#g" /etc/sentinel/${PORT}.conf
+sed -i "s#^dir .*#dir /var/sentinel/${PORT}#g" /etc/sentinel/${PORT}.conf
+
+# 5. 启动脚本
+redis-sentinel /etc/sentinel/${PORT}.conf
+
+# 6. 连接
+redis-cli -h 127.0.0.1 -p ${PORT}
+sentinel master mymaster
+sentinel slaves mymaster
+sentinel sentinels mymaster
+sentinel get-master-addr-by-name mymaster
+
+# 7. 删除产生的文件
+rm -rf /var/sentinel/${PORT}.conf /var/sentinel/${PORT} /var/run/sentinel_${PORT}.pid
