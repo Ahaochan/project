7b6fa4ec1351ee6c9061887b30d1612fae8d3d34
Ahaochan <844394093@qq.com>
2021-05-17 00:07:17 +0800

    🚧 working

--- ahao-spring-boot-elasticsearch/script/kibana/single.sh
+++ ahao-spring-boot-elasticsearch/script/kibana/single.sh
@@ -8,12 +8,15 @@ tar xvf kibana-7.8.1-linux-x86_64.tar.gz
 
 # 设置elasticsearch.url
 vim config/kibana.yml
+sed -i "s/#i18n.locale: \"en\"/i18n.locale: \"zh-CN\"/g" config/kibana.yml
 
 # 2. 启动 kibana
-bin/kibana
+# bin/kibana -e http://127.0.0.1:9200 -H 0.0.0.0 -p 5601
+touch kibana.log
+nohup bin/kibana -e http://127.0.0.1:9200 -H 0.0.0.0 -p 5601 > kibana.log 2>&1 &
 curl http://127.0.0.1:5601
 
 # 3. 安装插件
 bin/kibana-plugin list
 bin/kibana-plugin install plugin_location
-bin/kibana remove
+bin/kibana-plugin remove plugin_location
