7005d03b2f78900edfedf7b220391733aff3472b
Ahaochan <844394093@qq.com>
2021-08-14 20:34:56 +0800

    🚀 RDB备份脚本

---
+++ ahao-spring-boot-redission/script/bak_rdb_daily.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 0 0 * * * sh /usr/local/redis/copy/redis_rdb_copy_daily.sh /var/redis/6379/dump.rdb /usr/local/redis/snapshotting
+bak_dir=$2
+rdb_path=$1
+if [ ! -f "$rdb_path" ]; then
+    echo "$rdb_path不存在! 无法进行备份!";
+    exit 1;
+fi
+
+cur_yyyyMMddHH=$(date +%Y%m%d)
+rm -rf "${bak_dir:=/}/$cur_yyyyMMddHH"
+mkdir "${bak_dir:=/}/$cur_yyyyMMddHH"
+cp "$rdb_path" "${bak_dir:=/}/$cur_yyyyMMddHH"
+
+before_yyyyMMddHH=$(date -d -1month +%Y%m%d)
+rm -rf "${bak_dir:=/}/$before_yyyyMMddHH"
