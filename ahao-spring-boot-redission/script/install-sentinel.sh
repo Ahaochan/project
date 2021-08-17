65eaee6b1e79288ff112736621cedf832776636d
Ahaochan <844394093@qq.com>
2021-08-18 00:26:29 +0800

    🚀 优化sentinel脚本

--- ahao-spring-boot-redission/script/install-sentinel.sh
+++ ahao-spring-boot-redission/script/install-sentinel.sh
@@ -6,35 +6,51 @@ apt-get install tcl make gcc -y
 VERSION=6.2.2
 wget https://download.redis.io/releases/redis-${VERSION}.tar.gz
 tar xzf redis-${VERSION}.tar.gz
-cd redis-${VERSION}
+cd redis-${VERSION} || return 1
 make distclean && make && make test && make install
 
+# 3. 确保在 Redis 目录下
+if [ ! -f "redis.conf" ]; then
+    echo '请进入redis目录下'
+    return 1
+fi
+
 # 3. 开机时启动
-PORT=26379
+PORTS=(26379 26380 26381)
 
 # 4. 配置文件
-mkdir -p /etc/sentinel /var/sentinel/${PORT} /var/log/sentinel/${PORT}
-cp sentinel.conf /etc/sentinel/${PORT}.conf
-sed -i "s#^sentinel monitor mymaster .*#sentinel monitor mymaster 127.0.0.1 6379 2#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^sentinel down-after-milliseconds mymaster [[:digit:]]*#sentinel down-after-milliseconds mymaster 30001#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^sentinel parallel-syncs mymaster [[:digit:]]*#sentinel parallel-syncs mymaster 1#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^sentinel failover-timeout mymaster [[:digit:]]*#sentinel failover-timeout mymaster 180000#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^[# ]*bind .*#bind 127.0.0.1#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^port [[:digit:]]*#port ${PORT}#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^daemonize no#daemonize yes#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^pidfile /var/run/redis-sentinel[[:digit:]]*.pid#pidfile /var/run/redis-sentinel${PORT}.pid#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^logfile .*#/var/log/sentinel/${PORT}#g" /etc/sentinel/${PORT}.conf
-sed -i "s#^dir .*#dir /var/sentinel/${PORT}#g" /etc/sentinel/${PORT}.conf
+for ((i=0;i<${#PORTS[@]};i++))
+do
+    PORT=${PORTS[i]}
+    mkdir -p /etc/sentinel /var/sentinel/"${PORT}"
+    mkdir -p /var/log/sentinel && touch /var/log/sentinel/"${PORT}"
+    cp sentinel.conf /etc/sentinel/"${PORT}".conf
+    sed -i "s#^sentinel monitor mymaster .*#sentinel monitor mymaster 127.0.0.1 6379 2#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^sentinel down-after-milliseconds mymaster [[:digit:]]*#sentinel down-after-milliseconds mymaster 30000#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^sentinel parallel-syncs mymaster [[:digit:]]*#sentinel parallel-syncs mymaster 1#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^sentinel failover-timeout mymaster [[:digit:]]*#sentinel failover-timeout mymaster 180000#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^[# ]*bind .*#bind 127.0.0.1#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^port [[:digit:]]*#port ${PORT}#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^daemonize no#daemonize yes#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^pidfile /var/run/redis-sentinel[[:digit:]]*.pid#pidfile /var/run/redis-sentinel${PORT}.pid#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^logfile .*#logfile /var/log/sentinel/${PORT}#g" /etc/sentinel/"${PORT}".conf
+    sed -i "s#^dir .*#dir /var/sentinel/${PORT}#g" /etc/sentinel/"${PORT}".conf
 
-# 5. 启动脚本
-redis-sentinel /etc/sentinel/${PORT}.conf
+    # 5. 启动脚本
+    redis-sentinel /etc/sentinel/"${PORT}".conf
+done
 
 # 6. 连接
-redis-cli -h 127.0.0.1 -p ${PORT}
+redis-cli -h 127.0.0.1 -p 26379
 sentinel master mymaster
 sentinel slaves mymaster
 sentinel sentinels mymaster
 sentinel get-master-addr-by-name mymaster
 
 # 7. 删除产生的文件
-rm -rf /var/sentinel/${PORT}.conf /var/sentinel/${PORT} /var/run/sentinel_${PORT}.pid
+for ((i=0;i<${#PORTS[@]};i++))
+do
+    PORT=${PORTS[i]}
+    redis-cli -h 127.0.0.1 -p "${PORT}" SHUTDOWN
+    rm -rf /var/sentinel/"${PORT}".conf /var/sentinel/"${PORT}" /var/run/redis-sentinel"${PORT}".pid /var/log/sentinel/"${PORT}"
+done
