46acd843ec5ecb6d50041f11ba19802ef842888f
Ahaochan <844394093@qq.com>
2021-08-30 07:46:19 +0800

    ✨ 初始化zk节点

--- ahao-storm/src/main/java/moe/ahao/storm/product/bolt/HotProductCountBolt.java
+++ ahao-storm/src/main/java/moe/ahao/storm/product/bolt/HotProductCountBolt.java
@@ -1,23 +1,29 @@
 package moe.ahao.storm.product.bolt;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.trident.util.LRUMap;
 import org.apache.storm.tuple.Tuple;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
 public class HotProductCountBolt extends BaseRichBolt {
     private OutputCollector collector;
     private LRUMap<Long, Long> lru = new LRUMap<>(1000);
+    private CuratorFramework zk;
 
     @Override
     public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
         this.collector = collector;
+        this.zk = this.getZKClient("192.168.75.134:2181");
+        this.initZKNode(context.getThisTaskId());
     }
 
     @Override
@@ -25,7 +31,7 @@ public class HotProductCountBolt extends BaseRichBolt {
         Long id = input.getLongByField(LogParseBolt.FIELD);
 
         Long count = lru.get(id);
-        if(count == null) {
+        if (count == null) {
             count = 0L;
         }
         count++;
@@ -38,18 +44,47 @@ public class HotProductCountBolt extends BaseRichBolt {
     }
 
 
-    public class Task implements Callable<Map<Long, Long>> {
+    public class Task implements Runnable {
         @Override
-        public Map<Long, Long> call() throws Exception {
+        public void run() {
             while (!Thread.interrupted()) {
                 lru.entrySet().stream()
                     .sorted(Map.Entry.comparingByValue())
+                    .map(Map.Entry::getValue)
                     .limit(3)
                     .collect(Collectors.toList());
 
                 // TODO 上传到ZK
+                // zk.setData().withVersion(stat.getVersion()).forPath(path, newData);
             }
-            return null;
         }
     }
+
+    private void initZKNode(int taskId) {
+        String path = "/hot-product-" + taskId;
+
+        try {
+            Stat exists = zk.checkExists().forPath(path);
+            if (exists == null) {
+                zk.create()
+                    .creatingParentsIfNeeded()
+                    .withMode(CreateMode.EPHEMERAL)
+                    .forPath(path, new byte[0]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private CuratorFramework getZKClient(String connectString) {
+        CuratorFramework zk = CuratorFrameworkFactory.builder()
+            .connectString(connectString)
+            .sessionTimeoutMs(10000)
+            .namespace("hot-product-ns")
+            .build();
+        zk.start();
+        return zk;
+    }
+
 }
