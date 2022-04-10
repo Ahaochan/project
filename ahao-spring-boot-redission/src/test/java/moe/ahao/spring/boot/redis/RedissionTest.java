3e01584d3454ca002b043642d9d8a34656a94b47
Ahaochan <844394093@qq.com>
2022-04-10 09:56:24 +0800

    ✅ Redission高级特性

--- ahao-spring-boot-redission/src/test/java/moe/ahao/spring/boot/redis/RedissionTest.java
+++ ahao-spring-boot-redission/src/test/java/moe/ahao/spring/boot/redis/RedissionTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -99,6 +100,7 @@ class RedissionTest {
     }
 
     @Test
+    @Deprecated
     void redLock() throws Exception {
         RLock lock1 = redissonClient.getLock(REDIS_KEY + 1);
         RLock lock2 = redissonClient.getLock(REDIS_KEY + 2);
@@ -108,6 +110,71 @@ class RedissionTest {
 
         Assertions.assertTrue(redLock.tryLock(60, TimeUnit.SECONDS));
         redLock.unlock();
+    }
+
+    @Test
+    void readWriteLock() throws Exception {
+        RReadWriteLock readWriteLock = redissonClient.getReadWriteLock(REDIS_KEY);
+        RLock readLock = readWriteLock.readLock();
+        RLock writeLock = readWriteLock.writeLock();
+
+        writeLock.lock();
+        readLock.lock();
+        readLock.unlock();
+        writeLock.unlock();
+    }
+
+    @Test
+    void semaphore() throws Exception {
+        RSemaphore semaphore = redissonClient.getSemaphore(REDIS_KEY);
+        semaphore.trySetPermits(3);
+
+        int size = 10;
+        CountDownLatch latch = new CountDownLatch(10);
+        for (int i = 0; i < size; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println(new Date() + "：线程[" + Thread.currentThread().getName() + "]尝试获取Semaphore锁");
+                    semaphore.acquire();
+                    System.out.println(new Date() + "：线程[" + Thread.currentThread().getName() + "]成功获取到了Semaphore锁，开始工作");
+                    Thread.sleep(3000);
+                    semaphore.release();
+                    System.out.println(new Date() + "：线程[" + Thread.currentThread().getName() + "]释放Semaphore锁");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            }).start();
+        }
+        latch.await();
+
+        Assertions.assertEquals(0, latch.getCount());
+    }
+
+    @Test
+    void countDownLatch() throws Exception {
+        int size = 10;
+        RCountDownLatch latch = redissonClient.getCountDownLatch(REDIS_KEY);
+        latch.trySetCount(size);
+        for (int i = 0; i < size; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println(new Date() + "：线程[" + Thread.currentThread().getName() + "]开始工作");
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(new Date() + "：线程[" + Thread.currentThread().getName() + "]完成countDown");
+                    latch.countDown();
+                }
+            }).start();
+        }
+        latch.await();
+
+        Assertions.assertEquals(0, latch.getCount());
+
+        Thread.sleep(1000);
     }
 
     @Test
