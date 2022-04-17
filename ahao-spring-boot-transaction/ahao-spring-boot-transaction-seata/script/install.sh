3951b5d8626704e1004d807913a69466584aefc8
Ahaochan <844394093@qq.com>
2022-04-17 11:02:48 +0800

    🔧 seata server安装脚本优化

--- ahao-spring-boot-transaction/ahao-spring-boot-transaction-seata/script/install.sh
+++ ahao-spring-boot-transaction/ahao-spring-boot-transaction-seata/script/install.sh
@@ -5,11 +5,14 @@ tar zxvf seata-server-${VERSION}.tar.gz
 cd seata/ || exit
 
 # 修改JVM参数
-vim bin/seata-server.sh
+sed -i "s/Xmx[[:digit:]]*m/Xmx512m/g" bin/seata-server.sh
+sed -i "s/Xms[[:digit:]]*m/Xms512m/g" bin/seata-server.sh
+sed -i "s/Xmn[[:digit:]]*m/Xmn256m/g" bin/seata-server.sh
 
 # 启动
 mkdir logs && touch logs/seata_gc.log
 bin/seata-server.sh
 
 # 数据库添加undo_log表
-# https://ghproxy.com/https://github.com/seata/seata/blob/1.3.0/script/client/at/db/mysql.sql
+wget "https://ghproxy.com/https://raw.githubusercontent.com/seata/seata/develop/script/client/at/db/mysql.sql" -O /tmp/seata-at.sql
+mysql -h 127.0.0.1 -P 3306 -u root -proot -D ahaodb < /tmp/seata-at.sql
