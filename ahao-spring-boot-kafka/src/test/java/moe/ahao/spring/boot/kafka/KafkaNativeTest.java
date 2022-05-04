e0037e5c83210085a66bb622575324aea477447d
Ahaochan <844394093@qq.com>
2022-05-04 22:47:53 +0800

    ✅ 抽象EmbeddedKafkaTest, 作为Kafka单元测试基类

--- ahao-spring-boot-kafka/src/test/java/moe/ahao/spring/boot/kafka/KafkaNativeTest.java
+++ ahao-spring-boot-kafka/src/test/java/moe/ahao/spring/boot/kafka/KafkaNativeTest.java
@@ -1,17 +1,15 @@
 package moe.ahao.spring.boot.kafka;
 
+import moe.ahao.embedded.EmbeddedKafkaTest;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.clients.producer.*;
-import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigResource;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 
 import java.time.Duration;
 import java.util.*;
@@ -20,54 +18,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class KafkaNativeTest {
-    public static final String HOST = "127.0.0.1:9092";
-    public static final boolean AUTO_COMMIT = false;
 
-    private static EmbeddedKafkaBroker broker;
-    private static AdminClient adminClient;
-    public static final int SIZE = 100;
-    public static final int PARTITION = 2;
-
-    @BeforeAll
-    public static void beforeAll() throws Exception {
-        broker = new EmbeddedKafkaBroker(1).kafkaPorts(9092);
-        broker.afterPropertiesSet();
-
-        Map<String, Object> adminProperties = new HashMap<>();
-        adminProperties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, HOST);
-        adminClient = AdminClient.create(adminProperties);
-    }
-
-    @AfterAll
-    public static void afterAll() throws Exception {
-        if (adminClient != null) adminClient.close();
-        if (broker != null) broker.destroy();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        // 1. 生产消息
-        Map<String, Object> producerProperties = KafkaNativeTest.initProducerProperties();
-        // 2. 指定分区器
-        producerProperties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, SimplePartitioner.class.getName());
-        try (Producer<String, String> producer = new KafkaProducer<>(producerProperties);) {
-            for (int i = 0; i < SIZE; i++) {
-                String partitionKey = "key" + i;
-                String value = "value" + i;
-
-                ProducerRecord<String, String> record = new ProducerRecord<>(KafkaConfig.TOPIC_NAME, partitionKey, value);
-                producer.send(record, (recordMetadata, e) -> {
-                    if (e != null) {
-                        System.out.println("==================>" + "消息" + value + "发送失败");
-                        e.printStackTrace();
-                    } else {
-                        System.out.println("==================>" + "消息" + value + "发送成功, offset:" + recordMetadata.offset() + ", partition:" + recordMetadata.partition());
-                    }
-                });
-            }
-        }
-    }
+public class KafkaNativeTest extends EmbeddedKafkaTest {
 
     @ParameterizedTest
     @ValueSource(strings = "my-topic")
@@ -103,7 +55,7 @@ public class KafkaNativeTest {
     @Test
     public void consumerSync() throws Exception {
         // 1. 初始化消费者
-        Map<String, Object> prop = KafkaNativeTest.initConsumerProperties();
+        Map<String, Object> prop = EmbeddedKafkaTest.initConsumerProperties();
         Consumer<String, String> consumer = new KafkaConsumer<>(prop);
         consumer.subscribe(Arrays.asList(KafkaConfig.TOPIC_NAME)); // 订阅几个 topic
         // consumer.assign(Arrays.asList(new TopicPartition(KafkaConfig.TOPIC_NAME, 0))); // 订阅 topic 的某个 partition
@@ -200,52 +152,5 @@ public class KafkaNativeTest {
         // 3. 等待消息消费后, 再关闭资源
         boolean success = latch.await(10, TimeUnit.MINUTES);
         Assertions.assertTrue(success);
-    }
-
-    private static Map<String, Object> initProducerProperties() {
-        Map<String, Object> prop = new HashMap<>();
-        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, HOST);
-        prop.put(ProducerConfig.ACKS_CONFIG, "all");
-        prop.put(ProducerConfig.RETRIES_CONFIG, String.valueOf(0));
-        prop.put(ProducerConfig.BATCH_SIZE_CONFIG, String.valueOf(16384));
-        prop.put(ProducerConfig.LINGER_MS_CONFIG, String.valueOf(1));
-        prop.put(ProducerConfig.BUFFER_MEMORY_CONFIG, String.valueOf(33554432));
-        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return prop;
-    }
-
-    private static Map<String, Object> initConsumerProperties() {
-        Map<String, Object> prop = new HashMap<>();
-        prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, HOST);
-        prop.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
-        prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, String.valueOf(AUTO_COMMIT));
-        prop.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, String.valueOf(100));
-        prop.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, String.valueOf(15000));
-        prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        prop.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return prop;
-    }
-
-    public static class SimplePartitioner implements Partitioner {
-
-        @Override
-        public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
-            String keyStr = String.valueOf(key);
-            int index = Integer.parseInt(keyStr.substring(3)) % PARTITION;
-            System.out.println("==================>" + "[" + value + "]消息投递到第[" + index + "]个partition");
-            return index;
-        }
-
-        @Override
-        public void close() {
-
-        }
-
-        @Override
-        public void configure(Map<String, ?> configs) {
-
-        }
     }
 }
