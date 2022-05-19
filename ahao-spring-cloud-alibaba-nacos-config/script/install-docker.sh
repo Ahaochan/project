5e6e9e70fd8c740819ef41c4936d61d47766609c
Ahaochan <844394093@qq.com>
2022-05-19 19:08:53 +0800

    🐛 nacos2.0兼容性问题https://nacos.io/zh-cn/docs/2.0.0-compatibility.html

--- ahao-spring-cloud-alibaba-nacos-config/script/install-docker.sh
+++ ahao-spring-cloud-alibaba-nacos-config/script/install-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --name nacos-standalone -e MODE=standalone -p 8848:8848 -d nacos/nacos-server:latest
+docker run --name nacos-standalone -e MODE=standalone -p 8848:8848 -p 9848:9848 -p 9849:9849 -d nacos/nacos-server:latest
 # 控制台地址: http://127.0.0.1:8848/nacos/
 # 连接地址:   127.0.0.1:8848
