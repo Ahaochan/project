a7ea47aaaf556453187f2e6c6211c98423ec97c0
Ahaochan <844394093@qq.com>
2022-09-14 16:05:23 +0800

    🔊 skywalking日志接入

--- ahao-spring-boot-skywalking/script/run.sh
+++ ahao-spring-boot-skywalking/script/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# IDEA就复制到VM options里
 java -jar -javaagent:/opt/skywalking-agent.jar \
-     -Dskywalking.agent.service_name=AhaoWeb \
-     -Dskywalking.collector.backend_service=127.0.0.1:11800 \
+     -Dskywalking.agent.service_name=ahao-spring-boot-skywalking \
+     -Dskywalking.collector.backend_service=192.168.19.128:11800 \
+     -Dskywalking.plugin.toolkit.log.grpc.reporter.server_host=192.168.19.128 \
+     -Dskywalking.plugin.toolkit.log.grpc.reporter.server_port=11800 \
+     -Dskywalking.plugin.toolkit.log.grpc.reporter.max_message_size=10485760 \
+     -Dskywalking.plugin.toolkit.log.grpc.reporter.upstream_timeout=30 \
      ahao.jar --server.port=8080
--- ahao-spring-boot-skywalking/src/main/java/moe/ahao/spring/boot/skywalking/HelloController.java
+++ ahao-spring-boot-skywalking/src/main/java/moe/ahao/spring/boot/skywalking/HelloController.java
@@ -1,5 +1,7 @@
 package moe.ahao.spring.boot.skywalking;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,18 @@ import java.util.Date;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
     public String hello() {
-        return "now: " + new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String msg = "now: " + new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        logger.info("hello请求:{}", msg);
+        return msg;
     }
 
     @GetMapping("/exception")
     public String exception() {
+        logger.info("exception请求");
         throw new RuntimeException("exception");
     }
 }
