2b245ba37c3679f39545e750e629d7e610477d76
Ahaochan <844394093@qq.com>
2024-04-06 21:31:32 +0800

    unidbg

--- ahao-spring-unidbg-server/src/main/java/moe/ahao/unidbg/controller/CcController.java
+++ ahao-spring-unidbg-server/src/main/java/moe/ahao/unidbg/controller/CcController.java
@@ -1,16 +1,17 @@
 package moe.ahao.unidbg.controller;
 
-import moe.ahao.unidbg.vm.Te2VM;
+import moe.ahao.unidbg.vm.CcVM;
 import moe.ahao.util.commons.io.JSONHelper;
 import moe.ahao.util.commons.lang.CodecHelper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.StandardCharsets;
 
 @RestController
-public class Te2Controller {
+public class CcController {
     @PostMapping("/te2")
     public String te2(@RequestBody Object body) throws Exception {
         String arg1 = CodecHelper.parseBase64("MjRmZjdhMjU2ZDllY2Q3MTkyN2E1NTlhM2U4ZWQwYWJjZTQ0OTQ3ZmQ3YjA0ZTA3MDNhOTkzMDFiZmE2OWZhNjM4NjkwYzExZjYwZWMyZjBiMDcyYjA1MDBjMjhkYWEzY2RkM2U5MmNkZjViMjdhZjA0Y2MyYjg2NGM5NTYzMTgzMzdlYjg3ZTU4ZDQwMGQwMjljMTFlNmExNzcwMzU0NGMyZWYzZGUzMTBiYzFjNGRiYTU2ODU2MmEwOGY1NTUwZDcwYWQyYTMxYjViNTJhNjU1MjNiYjIzNDI0OTgxMWIxZjE3M2I2ZjU5NjY0NGViODgyZDM3MWY0MzRmYmY3OTllZGE0OTEwOTA5OTY5OTMwM2Q1YmE2ZTlhZWQwMWZjZWY2ZTc3OWFlZWRkMTZiMjE2MDA4NjI1MDg2N2IzYzRkMDAyNzA5NTQ5YzExNGIyNTFmYjk3MGIzZmJjMzkwYTlmODJkMDQ5");
@@ -18,8 +19,20 @@ public class Te2Controller {
         byte[] arg3 = CodecHelper.parseBase64("cWUzY2E4emU=").getBytes(StandardCharsets.UTF_8);
         String arg4 = JSONHelper.toString(body);
 
-        try (Te2VM test = new Te2VM()) {
-            String result = test.te2(arg1, arg2, arg3, arg4);
+        try (CcVM vm = new CcVM()) {
+            String result = vm.te2(arg1, arg2, arg3, arg4);
+            return result;
+        }
+    }
+    @PostMapping("/ad2")
+    public String ad2(@RequestParam String data) throws Exception {
+        String arg1 = CodecHelper.parseBase64("MjRmZjdhMjU2ZDllY2Q3MTkyN2E1NTlhM2U4ZWQwYWJjZTQ0OTQ3ZmQ3YjA0ZTA3MDNhOTkzMDFiZmE2OWZhNjM4NjkwYzExZjYwZWMyZjBiMDcyYjA1MDBjMjhkYWEzY2RkM2U5MmNkZjViMjdhZjA0Y2MyYjg2NGM5NTYzMTgzMzdlYjg3ZTU4ZDQwMGQwMjljMTFlNmExNzcwMzU0NGMyZWYzZGUzMTBiYzFjNGRiYTU2ODU2MmEwOGY1NTUwZDcwYWQyYTMxYjViNTJhNjU1MjNiYjIzNDI0OTgxMWIxZjE3M2I2ZjU5NjY0NGViODgyZDM3MWY0MzRmYmY3OTllZGE0OTEwOTA5OTY5OTMwM2Q1YmE2ZTlhZWQwMWZjZWY2ZTc3OWFlZWRkMTZiMjE2MDA4NjI1MDg2N2IzYzRkMDAyNzA5NTQ5YzExNGIyNTFmYjk3MGIzZmJjMzkwYTlmODJkMDQ5");
+        String arg2 = CodecHelper.parseBase64("a3JzejZyMzZYblRRY3VoSHB1SnJHTVJDZWE3b1ZNVE9jNlpaRnFLTnU2UT0=");
+        byte[] arg3 = null;
+        String arg4 = data;
+
+        try (CcVM vm = new CcVM()) {
+            String result = vm.ad2(arg1, arg2, arg3, arg4);
             return result;
         }
     }
--- ahao-spring-unidbg-server/src/main/java/moe/ahao/unidbg/vm/CcVM.java
+++ ahao-spring-unidbg-server/src/main/java/moe/ahao/unidbg/vm/CcVM.java
@@ -7,13 +7,12 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-public class Te2VM extends AndroidVM {
+public class CcVM extends AndroidVM {
     public static final String PATH = "ahao-spring-unidbg-server\\src\\main\\resources";
     public static final String SO = "libte2.so";
     public static final String PACKAGE = new String(Base64.getDecoder().decode("Y29tLmNjYi5sb25namlMaWZl"), StandardCharsets.UTF_8);
-    public static final String APK = "libte2.apk";
 
-    public Te2VM() throws IOException {
+    public CcVM() throws IOException {
         super(PACKAGE, false, true);
 
         // 加载目标SO
@@ -23,8 +22,18 @@ public class Te2VM extends AndroidVM {
     public String te2(String arg1, String arg2, byte[] arg3, String arg4) {
         String className = new String(Base64.getDecoder().decode("Y29tL2NjYi9jcnlwdG8vdHAvdG9vbC9UcFNhZmVVdGlscw=="), StandardCharsets.UTF_8);
 
-        DvmClass TpSafeUtils = vm.resolveClass(className);
-        StringObject stringObject = TpSafeUtils.callStaticJniMethodObject(emulator, "te2(Ljava/lang/String;Ljava/lang/String;[BLjava/lang/String;)Ljava/lang/String;", arg1, arg2, arg3, arg4);
+        DvmClass clazz = vm.resolveClass(className);
+        StringObject stringObject = clazz.callStaticJniMethodObject(emulator, "te2(Ljava/lang/String;Ljava/lang/String;[BLjava/lang/String;)Ljava/lang/String;", arg1, arg2, arg3, arg4);
+        String result = stringObject.getValue();
+
+        return result;
+    }
+
+    public String ad2(String arg1, String arg2, byte[] arg3, String arg4) {
+        String className = new String(Base64.getDecoder().decode("Y29tL2NjYi9jcnlwdG8vdHAvdG9vbC9UcFNhZmVVdGlscw=="), StandardCharsets.UTF_8);
+
+        DvmClass clazz = vm.resolveClass(className);
+        StringObject stringObject = clazz.callStaticJniMethodObject(emulator, "ad2(Ljava/lang/String;Ljava/lang/String;[BLjava/lang/String;)Ljava/lang/String;", arg1, arg2, arg3, arg4);
         String result = stringObject.getValue();
 
         return result;
