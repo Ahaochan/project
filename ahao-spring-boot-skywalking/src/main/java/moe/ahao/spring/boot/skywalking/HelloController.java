deea898b02644ceed7b9c17390494e7fd996a740
Ahaochan <844394093@qq,com>
2025-04-02 15:56:32 +0800

    skywalking 拦截对外请求

--- ahao-spring-boot-skywalking/src/main/java/moe/ahao/spring/boot/skywalking/HelloController.java
+++ ahao-spring-boot-skywalking/src/main/java/moe/ahao/spring/boot/skywalking/HelloController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -23,5 +24,13 @@ public class HelloController {
     public String exception() {
         logger.info("exception请求");
         throw new RuntimeException("exception");
+    }
+
+    @GetMapping("/ts")
+    public String ts() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://acs.m.taobao.com/gw/mtop.common.getTimestamp/";
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
     }
 }
