79c402d0753e3febc25bbdd9d99884d51d579792
Ahaochan <844394093@qq,com>
2025-04-07 14:12:21 +0800

    test(ai): 添加chatWithAssistant测试

--- ahao-spring-boot-ai/src/test/java/moe/ahao/spring/boot/ai/OpenAITest.java
+++ ahao-spring-boot-ai/src/test/java/moe/ahao/spring/boot/ai/OpenAITest.java
@@ -4,9 +4,11 @@ import com.openai.client.OpenAIClient;
 import com.openai.core.http.StreamResponse;
 import com.openai.models.ChatModel;
 import com.openai.models.chat.completions.ChatCompletion;
+import com.openai.models.chat.completions.ChatCompletionAssistantMessageParam;
 import com.openai.models.chat.completions.ChatCompletionChunk;
 import com.openai.models.chat.completions.ChatCompletionCreateParams;
 import moe.ahao.spring.boot.Starter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,8 +22,10 @@ public class OpenAITest {
     public void chat() throws Exception {
         ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
             .addSystemMessage("你会直接告诉答案")
-            .addUserMessage("美国的首都是哪里?")
+            .addUserMessage("你给我取一个中文名字，只用回答名字就好")
             .model(ChatModel.of("Qwen/QwQ-32B"))
+            .temperature(0.7)           // 增加温度会使输出更随机
+            .maxCompletionTokens(2000)  // 模型输出的最大的 token 数
             .build();
 
         ChatCompletion completion = openAIClient.chat().completions().create(params);
@@ -43,6 +47,22 @@ public class OpenAITest {
         System.out.println("提示词tokens: " + completion.usage().get().promptTokens());
         System.out.println("完成词tokens: " + completion.usage().get().completionTokens());
         System.out.println("总tokens: " + completion.usage().get().totalTokens());
+    }
+
+    @Test
+    public void chatWithAssistant() throws Exception {
+        ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
+            .addSystemMessage("你会直接告诉答案")
+            .addUserMessage("你给我取一个中文名字，只用回答名字就好")
+            .addMessage(ChatCompletionAssistantMessageParam.builder().content("小明").build())
+            .addUserMessage("你刚才给我取的名字是【小明】吗？只回答【true】或者【false】")
+            .model(ChatModel.of("Qwen/QwQ-32B"))
+            .build();
+        ChatCompletion completion = openAIClient.chat().completions().create(params);
+        String answer = completion.choices().get(0).message().content().get();
+
+        // 验证回答是否正确
+        Assertions.assertTrue(answer.contains("true"));
     }
 
     @Test
