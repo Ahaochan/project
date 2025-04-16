a25b348c8228b5d541b6986488ff2afdcfe8de65
Ahaochan <844394093@qq,com>
2025-04-16 17:41:51 +0800

    build(ahao-spring-boot-ai): 重构 OpenAI 相关配置和依赖
    - 移除自定义 OpenAIProperties 类，改为使用 Spring AI 提供的 OpenAiConnectionProperties
    - 更新 OpenAIConfig 类，使用新的 OpenAiConnectionProperties
    - 修改 application.properties 中的 OpenAI 配置项
    - 更新 pom.xml，添加 Spring Boot 和 Spring AI 依赖，移除冗余依赖

--- ahao-spring-boot-ai/src/main/java/moe/ahao/spring/boot/ai/config/OpenAIConfig.java
+++ ahao-spring-boot-ai/src/main/java/moe/ahao/spring/boot/ai/config/OpenAIConfig.java
@@ -2,7 +2,8 @@ package moe.ahao.spring.boot.ai.config;
 
 import com.openai.client.OpenAIClient;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
-import moe.ahao.spring.boot.ai.config.properties.OpenAIProperties;
+import org.springframework.ai.autoconfigure.openai.OpenAiConnectionProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAIConfig {
 
     @Bean(destroyMethod = "close")
-    public OpenAIClient client(OpenAIProperties geminiProperties) {
+    public OpenAIClient client(OpenAiConnectionProperties properties) {
         OpenAIClient client = OpenAIOkHttpClient.builder()
-            .baseUrl(geminiProperties.getBaseUrl())
-            .apiKey(geminiProperties.getApiKey())
-            .organization(geminiProperties.getOrganization())
-            .project(geminiProperties.getProject())
+            .baseUrl(properties.getBaseUrl())
+            .apiKey(properties.getApiKey())
+            .organization(properties.getOrganizationId())
+            .project(properties.getProjectId())
             .build();
         return client;
     }
